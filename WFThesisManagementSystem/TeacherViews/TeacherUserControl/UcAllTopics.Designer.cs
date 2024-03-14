81411684967c06f0f4d8ff027d0b13d60cd16ff2
kingioiloveyou <kientri098@gmail.com>
2024-03-14 13:11:44 +0700

    create alltopic

---
+++ WFThesisManagementSystem/TeacherViews/TeacherUserControl/UcAllTopics.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace WFThesisManagementSystem.TeacherViews.TeacherUserControl
+{
+    partial class UcAllTopics
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // UcAllTopics
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "UcAllTopics";
+            this.Size = new System.Drawing.Size(1266, 523);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
