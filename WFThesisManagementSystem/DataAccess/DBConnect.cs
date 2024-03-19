9a65a6a09638f79bec1184d88c7240836146e7ed
devkcng <kimmanhcuong230304@gmail.com>
2024-03-19 22:43:04 +0700

    Create DBConnect class for connect with data source

---
+++ WFThesisManagementSystem/DataAccess/DBConnect.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace WFThesisManagementSystem.DataAccess
+{
+    public class DBConnect
+    {
+        private const string DatabaseRelativePath = @"..\..\..\DATABASE\ThesisManagement.mdf";
+
+        private static readonly string ExecutablePath = AppDomain.CurrentDomain.BaseDirectory;
+
+        private static readonly string DatabaseFilePath = Path.GetFullPath(Path.Combine(ExecutablePath, DatabaseRelativePath));
+
+        private static readonly string ConnectionString = $"Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename={DatabaseFilePath};Integrated Security=True";
+        private readonly SqlConnection _conn = new SqlConnection(ConnectionString);
+
+        public bool ExecuteSqlQuery(string sqlStr)
+        {
+            try
+            {
+                _conn.Open();
+                var cmd = new SqlCommand(sqlStr, _conn);
+                return cmd.ExecuteNonQuery() > 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+            finally
+            {
+                _conn.Close();
+            }
+        }
+    }
+}
