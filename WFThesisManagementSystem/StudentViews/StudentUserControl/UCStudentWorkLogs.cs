86d1b90a027c95f2e67ccab135c1145fc26893fe
giahuynguyen28 <giahuy902@gmail.com>
2024-03-20 13:49:13 +0700

    Using the "Margin" property to  user controls.

--- WFThesisManagementSystem/StudentViews/StudentUserControl/UCStudentWorkLogs.cs
+++ WFThesisManagementSystem/StudentViews/StudentUserControl/UCStudentWorkLogs.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WFThesisManagementSystem.StudentViews.StudentUserControl;
 
 namespace WFThesisManagementSystem.Views.StudentViews
 {
@@ -15,6 +16,28 @@ namespace WFThesisManagementSystem.Views.StudentViews
         public UCStudentWorkLogs()
         {
             InitializeComponent();
+            addUserControl();
+            this.SizeChanged += UCStudentWorkLogs_SizeChanged;
+        }
+
+        private void UCStudentWorkLogs_SizeChanged(object sender, EventArgs e)
+        {
+            int marginX = flowLayoutPanelContainer.Width / 5;
+            foreach (UserControl uc in flowLayoutPanelContainer.Controls)
+            {
+                // Gán giá trị Margin cho UserControl
+                uc.Margin = new Padding(marginX, 10, marginX, 10);
+            }
+        }
+
+        private void addUserControl()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                UCWorkLog uc = new UCWorkLog();
+                flowLayoutPanelContainer.Controls.Add(uc);
+            }
         }
     }
+
 }
