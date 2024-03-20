92fbe9c38f93f2bd7b452374d41f4a88da14b985
devkcng <kimmanhcuong230304@gmail.com>
2024-03-20 17:26:34 +0700

    style: change type of function returing from bool to string

--- WFThesisManagementSystem/DataAccess/TopicDAO.cs
+++ WFThesisManagementSystem/DataAccess/TopicDAO.cs
@@ -1,31 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WFThesisManagementSystem.Models;
+﻿using WFThesisManagementSystem.Models;
 
 namespace WFThesisManagementSystem.DataAccess
 {
     internal class TopicDAO
     {
-        public bool Add(Topic topic, string Tablename)
+        public string AddTopic(Topic topic)
         {
-            DBConnect dbconnection = new DBConnect();
-            string SQL = string.Format("INSERT INTO {7}(topic_id , topic_name, topic_description,technology,requirement,category,number) VALUES ('{0}', '{1}' ,'{2}','{3}','{4}','{5}','{6}')", topic.Id, topic.Name, topic.Description, topic.Technology, topic.Requirement, topic.Category, topic.Number, Tablename);
-            return dbconnection.ExecuteSqlQuery(SQL);
+            return string.Format("INSERT INTO Topics (topic_id , topic_name, topic_description, topic_technology, topic_requirement, topic_category, max_members) " +
+                                 "VALUES ('{0}', '{1}' ,'{2}','{3}','{4}','{5}','{6}')",
+                                            topic.Id, topic.Name, topic.Description, topic.Technology, topic.Requirement, topic.Category, topic.MaxMember);
         }
-        public bool Delect(string id, string Tablename)
+        public string DeleteTopic(Topic topic)
         {
-            DBConnect dbconnection = new DBConnect();
-            string SQL = string.Format("DELETE FROM {1} WHERE topic_id = '{0}'", id, Tablename);
-            return dbconnection.ExecuteSqlQuery(SQL);
+            return string.Format("DELETE FROM Topics WHERE topic_id = '{0}'", topic.Id);
         }
-        public bool Fix(Topic topic, string Tablename)
+        public string UpdateTopic(Topic topic)
         {
-            DBConnect dbconnection = new DBConnect();
-            string SQL = string.Format("UPDATE {7} SET topic_name = '{0}', topic_description ='{1}', technology = '{2}', requirement= '{3}',category = '{4}',number = '{5}' WHERE topic_id = '{6}' ", topic.Name, topic.Description, topic.Technology, topic.Requirement, topic.Category, topic.Number, topic.Id, Tablename);
-            return dbconnection.ExecuteSqlQuery(SQL);
+            return string.Format("UPDATE Topics SET topic_name = '{0}', topic_description ='{1}', topic_technology = '{2}', topic_requirement= '{3}', topic_category = '{4}', max_members = '{5}' WHERE topic_id = '{6}' ",
+                                    topic.Name, topic.Description, topic.Technology, topic.Requirement, topic.Category, topic.MaxMember, topic.Id);
         }
     }
 }
