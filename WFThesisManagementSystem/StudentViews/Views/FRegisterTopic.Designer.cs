035aebd7f1c9ab71e7a45f8bb638d567beb50132
giahuynguyen28 <giahuy902@gmail.com>
2024-03-21 08:27:02 +0700

    create FStudentRegisterTopic form, add function FStudentRegisterTopic_Load (to load data from database) , add function dgvTopics_CellContentClick(click thì nhảy form đăng kí thành viên)

---
+++ WFThesisManagementSystem/StudentViews/Views/FRegisterTopic.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace WFThesisManagementSystem.StudentViews.Views
+{
+    partial class FRegisterTopic
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgvrRegisterMember = new Guna.UI2.WinForms.Guna2DataGridView();
+            this.student_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.student_id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtGroupName = new System.Windows.Forms.TextBox();
+            this.numericUpDownSize = new System.Windows.Forms.NumericUpDown();
+            this.lblProject = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvrRegisterMember)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSize)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvrRegisterMember
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
+            this.dgvrRegisterMember.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvrRegisterMember.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvrRegisterMember.ColumnHeadersHeight = 20;
+            this.dgvrRegisterMember.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            this.dgvrRegisterMember.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.student_name,
+            this.student_id});
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvrRegisterMember.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvrRegisterMember.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.dgvrRegisterMember.Location = new System.Drawing.Point(-1, 148);
+            this.dgvrRegisterMember.Name = "dgvrRegisterMember";
+            this.dgvrRegisterMember.RowHeadersVisible = false;
+            this.dgvrRegisterMember.RowHeadersWidth = 62;
+            this.dgvrRegisterMember.RowTemplate.Height = 28;
+            this.dgvrRegisterMember.Size = new System.Drawing.Size(977, 409);
+            this.dgvrRegisterMember.TabIndex = 0;
+            this.dgvrRegisterMember.ThemeStyle.AlternatingRowsStyle.BackColor = System.Drawing.Color.White;
+            this.dgvrRegisterMember.ThemeStyle.AlternatingRowsStyle.Font = null;
+            this.dgvrRegisterMember.ThemeStyle.AlternatingRowsStyle.ForeColor = System.Drawing.Color.Empty;
+            this.dgvrRegisterMember.ThemeStyle.AlternatingRowsStyle.SelectionBackColor = System.Drawing.Color.Empty;
+            this.dgvrRegisterMember.ThemeStyle.AlternatingRowsStyle.SelectionForeColor = System.Drawing.Color.Empty;
+            this.dgvrRegisterMember.ThemeStyle.BackColor = System.Drawing.Color.White;
+            this.dgvrRegisterMember.ThemeStyle.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.dgvrRegisterMember.ThemeStyle.HeaderStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.dgvrRegisterMember.ThemeStyle.HeaderStyle.BorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dgvrRegisterMember.ThemeStyle.HeaderStyle.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dgvrRegisterMember.ThemeStyle.HeaderStyle.ForeColor = System.Drawing.Color.White;
+            this.dgvrRegisterMember.ThemeStyle.HeaderStyle.HeaightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            this.dgvrRegisterMember.ThemeStyle.HeaderStyle.Height = 20;
+            this.dgvrRegisterMember.ThemeStyle.ReadOnly = false;
+            this.dgvrRegisterMember.ThemeStyle.RowsStyle.BackColor = System.Drawing.Color.White;
+            this.dgvrRegisterMember.ThemeStyle.RowsStyle.BorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.dgvrRegisterMember.ThemeStyle.RowsStyle.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dgvrRegisterMember.ThemeStyle.RowsStyle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            this.dgvrRegisterMember.ThemeStyle.RowsStyle.Height = 28;
+            this.dgvrRegisterMember.ThemeStyle.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.dgvrRegisterMember.ThemeStyle.RowsStyle.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            // 
+            // student_name
+            // 
+            this.student_name.HeaderText = "Student Name";
+            this.student_name.MinimumWidth = 8;
+            this.student_name.Name = "student_name";
+            // 
+            // student_id
+            // 
+            this.student_id.HeaderText = "Student ID";
+            this.student_id.MinimumWidth = 8;
+            this.student_id.Name = "student_id";
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(866, 563);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(110, 33);
+            this.btnRegister.TabIndex = 2;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F);
+            this.label1.Location = new System.Drawing.Point(3, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 27);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Group Name:";
+            // 
+            // txtGroupName
+            // 
+            this.txtGroupName.Location = new System.Drawing.Point(157, 105);
+            this.txtGroupName.Name = "txtGroupName";
+            this.txtGroupName.Size = new System.Drawing.Size(453, 26);
+            this.txtGroupName.TabIndex = 4;
+            // 
+            // numericUpDownSize
+            // 
+            this.numericUpDownSize.Location = new System.Drawing.Point(856, 103);
+            this.numericUpDownSize.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDownSize.Name = "numericUpDownSize";
+            this.numericUpDownSize.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDownSize.TabIndex = 6;
+            this.numericUpDownSize.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownSize.ValueChanged += new System.EventHandler(this.numericUpDownSize_ValueChanged);
+            // 
+            // lblProject
+            // 
+            this.lblProject.AutoSize = true;
+            this.lblProject.Font = new System.Drawing.Font("Arial", 12F);
+            this.lblProject.Location = new System.Drawing.Point(470, 33);
+            this.lblProject.Name = "lblProject";
+            this.lblProject.Size = new System.Drawing.Size(77, 27);
+            this.lblProject.TabIndex = 7;
+            this.lblProject.Text = "label2";
+            // 
+            // FRegisterTopic
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(988, 608);
+            this.Controls.Add(this.lblProject);
+            this.Controls.Add(this.numericUpDownSize);
+            this.Controls.Add(this.txtGroupName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.dgvrRegisterMember);
+            this.Name = "FRegisterTopic";
+            this.Text = "FRegisterTopic";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvrRegisterMember)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSize)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2DataGridView dgvrRegisterMember;
+        private System.Windows.Forms.DataGridViewTextBoxColumn student_name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn student_id;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtGroupName;
+        private System.Windows.Forms.NumericUpDown numericUpDownSize;
+        private System.Windows.Forms.Label lblProject;
+    }
+}
