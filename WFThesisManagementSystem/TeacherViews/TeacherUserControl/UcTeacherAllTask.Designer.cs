8c400c2cf1a100ee3a08d38bc179629647509cd4
kingioiloveyou <kientri098@gmail.com>
2024-03-21 13:19:36 +0700

    Thay Đổi kích thước task

--- WFThesisManagementSystem/TeacherViews/TeacherUserControl/UcTeacherAllTask.Designer.cs
+++ WFThesisManagementSystem/TeacherViews/TeacherUserControl/UcTeacherAllTask.Designer.cs
@@ -29,8 +29,8 @@
         private void InitializeComponent()
         {
             this.flpAllTasks = new System.Windows.Forms.FlowLayoutPanel();
-            this.ucTeacherSingleTask1 = new WFThesisManagementSystem.TeacherViews.TeacherUserControl.UcTeacherSingleTask();
             this.lblTasks = new System.Windows.Forms.Label();
+            this.ucTeacherSingleTask1 = new WFThesisManagementSystem.TeacherViews.TeacherUserControl.UcTeacherSingleTask();
             this.flpAllTasks.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -38,10 +38,20 @@
             // 
             this.flpAllTasks.AutoScroll = true;
             this.flpAllTasks.Controls.Add(this.ucTeacherSingleTask1);
-            this.flpAllTasks.Location = new System.Drawing.Point(198, 113);
+            this.flpAllTasks.Location = new System.Drawing.Point(129, 113);
             this.flpAllTasks.Name = "flpAllTasks";
-            this.flpAllTasks.Size = new System.Drawing.Size(1023, 567);
+            this.flpAllTasks.Size = new System.Drawing.Size(1378, 786);
             this.flpAllTasks.TabIndex = 6;
+            // 
+            // lblTasks
+            // 
+            this.lblTasks.AutoSize = true;
+            this.lblTasks.Font = new System.Drawing.Font("Century Gothic", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTasks.Location = new System.Drawing.Point(125, 36);
+            this.lblTasks.Name = "lblTasks";
+            this.lblTasks.Size = new System.Drawing.Size(98, 38);
+            this.lblTasks.TabIndex = 12;
+            this.lblTasks.Text = "Tasks";
             // 
             // ucTeacherSingleTask1
             // 
@@ -54,16 +64,6 @@
             this.ucTeacherSingleTask1.TabIndex = 0;
             this.ucTeacherSingleTask1.Time = null;
             // 
-            // lblTasks
-            // 
-            this.lblTasks.AutoSize = true;
-            this.lblTasks.Font = new System.Drawing.Font("Century Gothic", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTasks.Location = new System.Drawing.Point(194, 41);
-            this.lblTasks.Name = "lblTasks";
-            this.lblTasks.Size = new System.Drawing.Size(98, 38);
-            this.lblTasks.TabIndex = 12;
-            this.lblTasks.Text = "Tasks";
-            // 
             // UcTeacherAllTask
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
@@ -72,7 +72,7 @@
             this.Controls.Add(this.lblTasks);
             this.Controls.Add(this.flpAllTasks);
             this.Name = "UcTeacherAllTask";
-            this.Size = new System.Drawing.Size(1305, 787);
+            this.Size = new System.Drawing.Size(1584, 978);
             this.Load += new System.EventHandler(this.UcTeacherAllTask_Load);
             this.flpAllTasks.ResumeLayout(false);
             this.ResumeLayout(false);
