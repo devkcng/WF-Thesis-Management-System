890ab77c757012c6b7f98e3d64e10c2f993577f5
devkcng <kimmanhcuong230304@gmail.com>
2024-03-22 04:37:10 +0700

    style: rename textbox1 to txtDescribe, button1 to btnSearch

--- WFThesisManagementSystem/StudentViews/Views/FStudentRegisterTopic.Designer.cs
+++ WFThesisManagementSystem/StudentViews/Views/FStudentRegisterTopic.Designer.cs
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dgvTopics = new Guna.UI2.WinForms.Guna2DataGridView();
+            this.cbTopicTechnologyList = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.cbTeacherList = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.txtDescribe = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
             this.topic_id = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.topic_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.topic_description = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -41,26 +47,21 @@
             this.max_members = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.teacher_id = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.teacher_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.cbTopicTechnologyList = new Guna.UI2.WinForms.Guna2ComboBox();
-            this.cbTeacherList = new Guna.UI2.WinForms.Guna2ComboBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvTopics)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvTopics
             // 
-            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
-            this.dgvTopics.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dgvTopics.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.White;
+            this.dgvTopics.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle4;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvTopics.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
             this.dgvTopics.ColumnHeadersHeight = 38;
             this.dgvTopics.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
             this.dgvTopics.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
@@ -73,22 +74,23 @@
             this.max_members,
             this.teacher_id,
             this.teacher_name});
-            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
-            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
-            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
-            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dgvTopics.DefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvTopics.DefaultCellStyle = dataGridViewCellStyle6;
             this.dgvTopics.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.dgvTopics.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
-            this.dgvTopics.Location = new System.Drawing.Point(0, 156);
+            this.dgvTopics.Location = new System.Drawing.Point(0, 113);
+            this.dgvTopics.Margin = new System.Windows.Forms.Padding(2);
             this.dgvTopics.Name = "dgvTopics";
             this.dgvTopics.RowHeadersVisible = false;
             this.dgvTopics.RowHeadersWidth = 62;
             this.dgvTopics.RowTemplate.Height = 28;
-            this.dgvTopics.Size = new System.Drawing.Size(1077, 441);
+            this.dgvTopics.Size = new System.Drawing.Size(718, 318);
             this.dgvTopics.TabIndex = 0;
             this.dgvTopics.ThemeStyle.AlternatingRowsStyle.BackColor = System.Drawing.Color.White;
             this.dgvTopics.ThemeStyle.AlternatingRowsStyle.Font = null;
@@ -112,6 +114,81 @@
             this.dgvTopics.ThemeStyle.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
             this.dgvTopics.ThemeStyle.RowsStyle.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
             this.dgvTopics.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvTopics_CellContentClick_1);
+            // 
+            // cbTopicTechnologyList
+            // 
+            this.cbTopicTechnologyList.BackColor = System.Drawing.Color.Transparent;
+            this.cbTopicTechnologyList.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.cbTopicTechnologyList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbTopicTechnologyList.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.cbTopicTechnologyList.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.cbTopicTechnologyList.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.cbTopicTechnologyList.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
+            this.cbTopicTechnologyList.ItemHeight = 30;
+            this.cbTopicTechnologyList.Location = new System.Drawing.Point(484, 73);
+            this.cbTopicTechnologyList.Margin = new System.Windows.Forms.Padding(2);
+            this.cbTopicTechnologyList.Name = "cbTopicTechnologyList";
+            this.cbTopicTechnologyList.Size = new System.Drawing.Size(119, 36);
+            this.cbTopicTechnologyList.TabIndex = 2;
+            // 
+            // cbTeacherList
+            // 
+            this.cbTeacherList.BackColor = System.Drawing.Color.Transparent;
+            this.cbTeacherList.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.cbTeacherList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbTeacherList.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.cbTeacherList.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.cbTeacherList.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.cbTeacherList.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
+            this.cbTeacherList.ItemHeight = 30;
+            this.cbTeacherList.Location = new System.Drawing.Point(602, 73);
+            this.cbTeacherList.Margin = new System.Windows.Forms.Padding(2);
+            this.cbTeacherList.Name = "cbTeacherList";
+            this.cbTeacherList.Size = new System.Drawing.Size(119, 36);
+            this.cbTeacherList.TabIndex = 3;
+            this.cbTeacherList.SelectedIndexChanged += new System.EventHandler(this.cbTeacherList_SelectedIndexChanged);
+            // 
+            // txtDescribe
+            // 
+            this.txtDescribe.Location = new System.Drawing.Point(135, 61);
+            this.txtDescribe.Margin = new System.Windows.Forms.Padding(2);
+            this.txtDescribe.Multiline = true;
+            this.txtDescribe.Name = "txtDescribe";
+            this.txtDescribe.Size = new System.Drawing.Size(304, 48);
+            this.txtDescribe.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(-5, 71);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Describe topic";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(306, 6);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 32);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Topic List";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackgroundImage = global::WFThesisManagementSystem.Properties.Resources.Search1;
+            this.btnSearch.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnSearch.Location = new System.Drawing.Point(437, 62);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(47, 47);
+            this.btnSearch.TabIndex = 7;
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
             // 
             // topic_id
             // 
@@ -161,6 +238,7 @@
             this.teacher_id.HeaderText = "Teacher ID";
             this.teacher_id.MinimumWidth = 8;
             this.teacher_id.Name = "teacher_id";
+            this.teacher_id.Visible = false;
             // 
             // teacher_name
             // 
@@ -168,76 +246,19 @@
             this.teacher_name.MinimumWidth = 8;
             this.teacher_name.Name = "teacher_name";
             // 
-            // cbTopicTechnologyList
-            // 
-            this.cbTopicTechnologyList.BackColor = System.Drawing.Color.Transparent;
-            this.cbTopicTechnologyList.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
-            this.cbTopicTechnologyList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cbTopicTechnologyList.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.cbTopicTechnologyList.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.cbTopicTechnologyList.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.cbTopicTechnologyList.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
-            this.cbTopicTechnologyList.ItemHeight = 30;
-            this.cbTopicTechnologyList.Location = new System.Drawing.Point(719, 114);
-            this.cbTopicTechnologyList.Name = "cbTopicTechnologyList";
-            this.cbTopicTechnologyList.Size = new System.Drawing.Size(176, 36);
-            this.cbTopicTechnologyList.TabIndex = 2;
-            // 
-            // cbTeacherList
-            // 
-            this.cbTeacherList.BackColor = System.Drawing.Color.Transparent;
-            this.cbTeacherList.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
-            this.cbTeacherList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cbTeacherList.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.cbTeacherList.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.cbTeacherList.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.cbTeacherList.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
-            this.cbTeacherList.ItemHeight = 30;
-            this.cbTeacherList.Location = new System.Drawing.Point(901, 114);
-            this.cbTeacherList.Name = "cbTeacherList";
-            this.cbTeacherList.Size = new System.Drawing.Size(176, 36);
-            this.cbTeacherList.TabIndex = 3;
-            this.cbTeacherList.SelectedIndexChanged += new System.EventHandler(this.cbTeacherList_SelectedIndexChanged);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(202, 85);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(486, 65);
-            this.textBox1.TabIndex = 4;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(-7, 98);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(203, 38);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Describe topic";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(459, 9);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(177, 48);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Topic List";
-            // 
             // FStudentRegisterTopic
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1077, 597);
+            this.ClientSize = new System.Drawing.Size(718, 431);
+            this.Controls.Add(this.btnSearch);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtDescribe);
             this.Controls.Add(this.cbTeacherList);
             this.Controls.Add(this.cbTopicTechnologyList);
             this.Controls.Add(this.dgvTopics);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "FStudentRegisterTopic";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FStudentRegisterTopic";
@@ -253,6 +274,10 @@
         private Guna.UI2.WinForms.Guna2DataGridView dgvTopics;
         private Guna.UI2.WinForms.Guna2ComboBox cbTopicTechnologyList;
         private Guna.UI2.WinForms.Guna2ComboBox cbTeacherList;
+        private System.Windows.Forms.TextBox txtDescribe;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSearch;
         private System.Windows.Forms.DataGridViewTextBoxColumn topic_id;
         private System.Windows.Forms.DataGridViewTextBoxColumn topic_name;
         private System.Windows.Forms.DataGridViewTextBoxColumn topic_description;
@@ -262,8 +287,5 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn max_members;
         private System.Windows.Forms.DataGridViewTextBoxColumn teacher_id;
         private System.Windows.Forms.DataGridViewTextBoxColumn teacher_name;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
     }
 }
