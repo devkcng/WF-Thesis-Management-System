89071b5607b3f82161842ff2ae3d6fec5f5722d8
devkcng <kimmanhcuong230304@gmail.com>
2024-03-22 04:37:25 +0700

    feat: add search engine

---
+++ WFThesisManagementSystem/Utilities/SearchEngine/Document.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WFThesisManagementSystem.Utilities.SearchEngine
+{
+    public class Document
+    {   
+        public string TopicId { get; set; }
+        public string TeacherName { get; set; }
+
+        public string TeacherId { get; set; }
+
+        public string TopicName { get; set; }
+        public string TopicDescription { get; set; }
+        public string TopicRequirement { get; set; }
+        public string TopicCategory { get; set; }
+        public string TopicTechnology { get; set; }
+
+        public string TopicMaxMembers { get; set; }
+
+        // Constructor to initialize properties
+        public Document () { }
+
+        public Document (string topicId, string teacherName, string teacherId , string topicName, string topicDescription, string topicRequirement, string topicCategory, string topicTechnology, string topicMaxMembers)
+        {
+            TopicId = topicId;
+            TeacherName = teacherName;
+            TeacherId = teacherId;
+            TopicName = topicName;
+            TopicDescription = topicDescription;
+            TopicRequirement = topicRequirement;
+            TopicCategory = topicCategory;
+            TopicTechnology = topicTechnology;
+            TopicMaxMembers = topicMaxMembers;
+        }
+    }
+}
