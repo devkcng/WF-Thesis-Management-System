eaedeb71d48fc062f03e1dc191aacc018f55a103
kingioiloveyou <kientri098@gmail.com>
2024-03-23 22:25:34 +0700

    create task and taskDao before ( do not use)

---
+++ WFThesisManagementSystem/DataAccess/TaskDAO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WFThesisManagementSystem.Models;
+using Task = WFThesisManagementSystem.Models.Task;
+
+namespace WFThesisManagementSystem.DataAccess
+{
+    internal class TaskDAO
+    {
+        public string AddTask(Task task)
+        {
+            return string.Format("INSERT INTO Tasks (topic_id , topic_name, topic_description, topic_technology, topic_requirement, topic_category, max_members) " +
+                                 "VALUES ('{0}', '{1}' ,'{2}','{3}','{4}','{5}','{6}')",
+                                            task.Id, task.TopicId,task.NameTeacher,task.NameTopic);
+        }
+        public string DeleteTask(Task task)
+        {
+            return string.Format("DELETE FROM Tasks WHERE topic_id = '{0}'", task.Id);
+        }
+        public string UpdateTask(Task task)
+        {
+            return string.Format("UPDATE Tasks SET topic_name = '{0}', topic_description ='{1}', topic_technology = '{2}', topic_requirement= '{3}', topic_category = '{4}', max_members = '{5}' WHERE topic_id = '{6}' ",
+                                     task.Id, task.TopicId, task.NameTeacher, task.NameTopic);
+        }
+    }
+}
---
+++ WFThesisManagementSystem/Models/Task.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WFThesisManagementSystem.Models
+{
+    internal class Task
+    {
+        public int Id { get; set; }
+        public string NameTeacher { get; set; }
+
+        public string NameTopic { get; set; }
+        public string TopicId { get; set; } 
+
+        public Task() {  }
+
+        public Task(int id, string nameTeacher, string nameTopic, string topicId)
+        {
+            Id = id;
+            NameTeacher = nameTeacher;
+            NameTopic = nameTopic;
+            TopicId = topicId;
+        }
+    }
+}
