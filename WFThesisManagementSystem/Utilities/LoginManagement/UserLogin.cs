054b1ecf017b8484143a70abbaf27111520b865b
kingioiloveyou <kientri098@gmail.com>
2024-03-25 20:10:00 +0700

    Thêm 2 biến static username và  password vào trong userlogin

--- WFThesisManagementSystem/Utilities/LoginManagement/UserLogin.cs
+++ WFThesisManagementSystem/Utilities/LoginManagement/UserLogin.cs
@@ -10,6 +10,8 @@ namespace WFThesisManagementSystem.Utilities.LoginManagement
 {
     public class UserLogin
     {
+        public static string UserName { get; set; }
+        public static string Password { get; set; }
         public bool Login(string username, string password)
         {
             DBConnect dBConnect = new DBConnect();
