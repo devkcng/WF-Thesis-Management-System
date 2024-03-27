61cb7bc8522cdb95cab5d08c0e34b35ae9607b17
kingioiloveyou <kientri098@gmail.com>
2024-03-27 21:52:52 +0700

    tạo class để query regist

---
+++ WFThesisManagementSystem/DataAccess/RegistDAO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WFThesisManagementSystem.DataAccess
+{
+    internal class RegistDAO
+    {
+        public static string UpdateAccept(string id) => string.Format($"UPDATE RegisterQueue SET accepted = 1 WHERE student_id ={id} AND accepted = 0 ");
+        public static string UpdateGroupid(string id,string group_id)
+        {
+            return string.Format($"UPDATE Student SET group_id = {group_id} WHERE student_id ={id} ");
+        }
+    }
+
+}
