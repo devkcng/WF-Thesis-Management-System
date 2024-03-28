6653e74d7a00dea0945f90f2c0f111a9379d4f93
devkcng <kimmanhcuong230304@gmail.com>
2024-03-28 08:16:36 +0700

    style: change default location to center screen

--- WFThesisManagementSystem/TeacherViews/Views/FTeacherEditTopic.Designer.cs
+++ WFThesisManagementSystem/TeacherViews/Views/FTeacherEditTopic.Designer.cs
@@ -34,18 +34,21 @@
             // ucTeacherEditTopic1
             // 
             this.ucTeacherEditTopic1.BackColor = System.Drawing.Color.Pink;
-            this.ucTeacherEditTopic1.Location = new System.Drawing.Point(-1, -4);
+            this.ucTeacherEditTopic1.Location = new System.Drawing.Point(-1, -3);
+            this.ucTeacherEditTopic1.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.ucTeacherEditTopic1.Name = "ucTeacherEditTopic1";
-            this.ucTeacherEditTopic1.Size = new System.Drawing.Size(911, 993);
+            this.ucTeacherEditTopic1.Size = new System.Drawing.Size(607, 645);
             this.ucTeacherEditTopic1.TabIndex = 0;
             // 
             // FTeacherEditTopic
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(908, 989);
+            this.ClientSize = new System.Drawing.Size(605, 643);
             this.Controls.Add(this.ucTeacherEditTopic1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FTeacherEditTopic";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FTeacherEditTopic";
             this.ResumeLayout(false);
 
