b4239f26fd8ddfe0d57dbc0a76495acaedd7e419
devkcng <kimmanhcuong230304@gmail.com>
2024-04-02 00:22:49 +0700

    feat: add ValidationInfoHelper.cs class

---
+++ WFThesisManagementSystem/Helper/ValidationInformation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WFThesisManagementSystem.Helper
+{
+    public class ValidationInformation
+    {   
+        //check if the string is a number
+        public bool CheckTopicId(string topicId)
+        {
+            for (int i = 0; i <= topicId.Length - 1; i++)
+            {
+                if (topicId[i] > '9' || topicId[i] < '0')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        //email validation, with popular regex
+        public bool CheckEmail(string email)
+        {
+            return System.Text.RegularExpressions.Regex.IsMatch(email, @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+        }
+    }
+}
