3d35764f806c75f1e02ad13c24b10c43ed294ab3
giahuynguyen28 <giahuy902@gmail.com>
2024-04-02 20:53:37 +0700

    Create and design sub Task usercontrol

---
+++ WFThesisManagementSystem/Forms/StudentViews/StudentUserControl/UCStudentSubTask.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFThesisManagementSystem.Forms.StudentViews.StudentUserControl
+{
+    public partial class UCStudentSubTask : UserControl
+    {
+        public UCStudentSubTask()
+        {
+            InitializeComponent();
+            addUserControl();
+        }
+
+        private void addUserControl()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                UCSubTask uc = new UCSubTask();
+                flpAllSubTasksView.Controls.Add(uc);
+
+                UCStudentInfo ucstudentinfo = new UCStudentInfo();
+                flpMemberGroupView.Controls.Add(ucstudentinfo);
+            }
+        }
+    }
+}
