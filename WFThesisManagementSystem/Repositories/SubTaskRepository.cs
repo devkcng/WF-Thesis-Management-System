e1ce8a6d254e4c36b4384cd27e17055bd224528b
devkcng <kimmanhcuong230304@gmail.com>
2024-04-02 14:48:00 +0700

    feat: add TaskRepository and SubTaskRepository to project

---
+++ WFThesisManagementSystem/Repositories/SubTaskRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WFThesisManagementSystem.Models;
+
+namespace WFThesisManagementSystem.Repositories
+{
+    public class SubTaskRepository : IRepository<SubTask>
+    {   
+        private ThesisManagementContext _context;
+
+        public SubTaskRepository(ThesisManagementContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<SubTask> GetAll()
+        {
+            return _context.SubTasks;
+        }
+
+        public SubTask GetById(int id)
+        {
+            return _context.SubTasks.FirstOrDefault(x => x.subtask_id == id);
+        }
+
+        public void Add(SubTask entity)
+        {
+            _context.SubTasks.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(SubTask entity)
+        {
+            _context.Entry(entity).State = System.Data.Entity.EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void Delete(SubTask entity)
+        {
+            _context.SubTasks.Remove(entity);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ WFThesisManagementSystem/Repositories/TaskRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WFThesisManagementSystem.Models;
+
+namespace WFThesisManagementSystem.Repositories
+{
+    public class TaskRepository : IRepository<Task>
+    {   
+        private ThesisManagementContext _context;
+
+        public TaskRepository(ThesisManagementContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<Task> GetAll()
+        {
+           return _context.Tasks;
+        }
+
+        public Task GetById(int id)
+        {
+            return _context.Tasks.FirstOrDefault(x => x.task_id == id);
+        }
+
+        public void Add(Task entity)
+        {
+            _context.Tasks.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(Task entity)
+        {
+            _context.Entry(entity).State = System.Data.Entity.EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void Delete(Task entity)
+        {
+           _context.Tasks.Remove(entity);
+           _context.SaveChanges();
+        }
+    }
+}
