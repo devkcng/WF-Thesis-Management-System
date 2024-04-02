984a2ad6428a57a5c4843481b4fca36569d2e8b7
giahuynguyen28 <giahuy902@gmail.com>
2024-04-02 20:52:58 +0700

    Create and design Invidual Task usercontrol

---
+++ WFThesisManagementSystem/Forms/StudentViews/StudentUserControl/UCInvidualTask.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFThesisManagementSystem.Forms.StudentViews.StudentUserControl
+{
+    public partial class UCInvidualTask : UserControl
+    {
+        public UCInvidualTask()
+        {
+            InitializeComponent();
+        }
+    }
+}
