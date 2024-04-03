39cbacabb676d482a024407b824c42ab37ecf4b0
devkcng <kimmanhcuong230304@gmail.com>
2024-04-03 17:03:48 +0700

    fix: fix can't delete and update Task

--- WFThesisManagementSystem/Forms/TeacherViews/Views/FTeacherEditTask.cs
+++ WFThesisManagementSystem/Forms/TeacherViews/Views/FTeacherEditTask.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
 using WFThesisManagementSystem.Models;
-using WFThesisManagementSystem.Repositories;
 using WFThesisManagementSystem.Services;
 
 namespace WFThesisManagementSystem.Forms.TeacherViews.Views
@@ -9,35 +8,68 @@ namespace WFThesisManagementSystem.Forms.TeacherViews.Views
     public partial class FTeacherEditTask : Form
     {
         private Task _task;
-        private TaskService _taskService;
-        public FTeacherEditTask(Task task)
+        private ThesisManagementContext _context;
+        public FTeacherEditTask(Task task, ThesisManagementContext context)
         {
             InitializeComponent();
-            var _context = new ThesisManagementContext();
+            _context = context;
             _task = task;
-            _taskService = new TaskService(task);
-            ucTeacherEditTask1.btnClose.Click += Close;
-            ucTeacherEditTask1.btnUpdate.Click += Update;
-            ucTeacherEditTask1.btnDelete.Click += Delete;
+            ucTeacherEditTask1.DeleteButtonClicked += Delete;
+            ucTeacherEditTask1.UpdateButtonClicked += Update;
+            ucTeacherEditTask1.CloseButtonClicked += Close;
         }
-        
-        private void Close(object sender, EventArgs e) { 
-            this.Hide(); }
-        private void Update(object sender, EventArgs e) 
+
+        private void Close(object sender, EventArgs e)
         {
-            _taskService.UpdateTask();
-            //_taskRepository.UpdateTask(_task);
+            this.Close();
         }
-        private void Delete(object sender, EventArgs e) 
-        { 
-            _taskService.DeleteTask();
+        private void Update(object sender, EventArgs e)
+        {
+            try
+            {
+
+                var task = new Task
+                {
+                    task_id = _task.task_id,
+                    task_name = ucTeacherEditTask1.txtTaskName.Text,
+                    task_description = ucTeacherEditTask1.txtTaskDescription.Text,
+                    open_day = _task.open_day,
+                    due_date = ucTeacherEditTask1.dtpDueDate.Value,
+                    submit_day = _task.submit_day,
+                    group_id = _task.group_id
+                };
+
+                var taskService = new TaskService(task, _context);
+
+                if (taskService.UpdateTask())
+                {
+                    MessageBox.Show("Task updated successfully");
+
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Task update failed");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        private void Delete(object sender, EventArgs e)
+        {
+            var taskService = new TaskService(_task, _context);
+            taskService.DeleteTask();
+            this.Close();
         }
 
         private void FTeacherEditTask_Load_1(object sender, EventArgs e)
-        {
+        {   
             ucTeacherEditTask1.txtTaskDescription.Text = _task.task_description;
             ucTeacherEditTask1.txtTaskName.Text = _task.task_name;
-            ucTeacherEditTask1.dtpEndDate.Value = _task.due_date.Value;
+            ucTeacherEditTask1.dtpDueDate.Value = _task.due_date.Value;
         }
     }
 }
