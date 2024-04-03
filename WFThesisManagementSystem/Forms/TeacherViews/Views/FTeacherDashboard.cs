0980d5de7b63c997a3d93afde55cc2fd3ffbf743
KienLoveNH <kientri098@gmail.com>
2024-04-03 09:48:39 -0700

    Thêm những hàm đã copy từ bên feature 2 task

--- WFThesisManagementSystem/Forms/TeacherViews/Views/FTeacherDashboard.cs
+++ WFThesisManagementSystem/Forms/TeacherViews/Views/FTeacherDashboard.cs
@@ -1,12 +1,22 @@
 ﻿using ComponentFactory.Krypton.Toolkit;
 using System;
+using WFThesisManagementSystem.Forms.TeacherViews.TeacherUserControl;
+using WFThesisManagementSystem.Models;
+using WFThesisManagementSystem.Repositories;
 
 namespace WFThesisManagementSystem.Forms.TeacherViews.Views
 {
     public partial class FTeacherDashboard : KryptonForm
     {
+        ThesisManagementContext _context;
+        StudentGroupRepository _studentGroupRepository;
+        TaskRepository _taskRepository;
+        int GroupIdCreate;
         public FTeacherDashboard()
         {
+            _context = new ThesisManagementContext();
+            _studentGroupRepository = new StudentGroupRepository(_context);
+            _taskRepository = new TaskRepository(_context);
             InitializeComponent();
             ucTeacherAllTopics1.Hide();
             ucTeacherAllTask1.Hide();
@@ -26,7 +36,7 @@ namespace WFThesisManagementSystem.Forms.TeacherViews.Views
         private void createTask(object sender, EventArgs e)
         {
             ucTeacherAllTask1.Hide();
-            FTeacherCreateTask fTeacherCreateTask = new FTeacherCreateTask();
+            FTeacherCreateTask fTeacherCreateTask = new FTeacherCreateTask(GroupIdCreate);
             fTeacherCreateTask.Show();
         }
 
@@ -37,10 +47,13 @@ namespace WFThesisManagementSystem.Forms.TeacherViews.Views
 
         private void task_Click(object sender, EventArgs e)
         {
+            
             ucTeacherAllTopics1.Hide();
             ucTeacherAllTask1.Show();
             ucTeacherEditInfor1.Hide();
             ucTeacherInfor1.Hide();
+            ucTeacherAllTask1_Load(sender, e);
+
         }
 
         private void logout_Click(object sender, EventArgs e)
@@ -74,5 +87,70 @@ namespace WFThesisManagementSystem.Forms.TeacherViews.Views
             ucTeacherInfor1.Show();
             ucTeacherEditInfor1.Show();
         }
+        private void ucTeacherAllTask1_Load(object sender, EventArgs e)
+        {
+            ListGroup();
+        }
+        #region ucTeacherAllTask1-Components
+
+        private void ListGroup()
+        {
+            ucTeacherAllTask1.flpAllGroupView.Controls.Clear();
+            var studentGroupList = _studentGroupRepository.GetAll();
+            foreach (var studentGroup in studentGroupList)
+            {
+                UcTeacherSingleGroup ucTeacherSingleGroup = new UcTeacherSingleGroup();
+                ucTeacherSingleGroup.Name = studentGroup.group_name;
+                ucTeacherSingleGroup.Id = studentGroup.group_id;
+                ucTeacherSingleGroup.Clicked += ucTeacherSingleGroup_Clicked;
+                ucTeacherAllTask1.flpAllGroupView.Controls.Add(ucTeacherSingleGroup);
+            }
+        }
+        #endregion
+
+        #region ucTeacherSingleGroup-Events
+
+        // when click on a group user control -> show tasks of that group
+
+        private void ucTeacherSingleGroup_Clicked(object sender, EventArgs e)
+        {
+            UcTeacherSingleGroup ucTeacherSingleGroup = sender as UcTeacherSingleGroup;
+            int groupId = GroupIdCreate = ucTeacherSingleGroup.Id;
+            var taskList = _taskRepository.GetTaskByGroupId(groupId);
+            ucTeacherAllTask1.flpAllTasks.Controls.Clear();
+            foreach (var task in taskList)
+            {
+                UcTeacherSingleTask ucTeacherSingleTask = new UcTeacherSingleTask();
+                ucTeacherSingleTask.Name = task.task_name;
+                ucTeacherSingleTask.TimeOpen = task.open_day.Value;
+                ucTeacherSingleTask.TimeEnd = task.due_date.Value;
+                ucTeacherSingleTask.EditButtonClicked += ucTeacherSingleTask_EditButton_Click;
+                ucTeacherSingleTask.SubmitButtonClicked += ucTeacherSingleTask_SubmitTask_Click;
+                ucTeacherAllTask1.flpAllTasks.Controls.Add(ucTeacherSingleTask);
+            }
+        }
+
+        #endregion
+
+        #region UcTeacherSingleTask-Events
+
+        private void ucTeacherSingleTask_EditButton_Click(object sender, EventArgs e)
+        {
+            UcTeacherSingleTask ucTeacherSingleTask = sender as UcTeacherSingleTask;
+            var taskName = ucTeacherSingleTask.Name;
+            var task = _taskRepository.GetTaskByTaskName(taskName);
+            FTeacherEditTask fTeacherEditTask = new FTeacherEditTask(task);
+            fTeacherEditTask.Show();
+        }
+        private void ucTeacherSingleTask_SubmitTask_Click(object sender, EventArgs e) 
+        {
+            UcTeacherSingleTask ucTeacherSingleTask = sender as UcTeacherSingleTask;
+            var taskName = ucTeacherSingleTask.Name;
+            var task = _taskRepository.GetTaskByTaskName(taskName);
+            //task. = 
+        }
+
+        #endregion
+       
     }
 }
