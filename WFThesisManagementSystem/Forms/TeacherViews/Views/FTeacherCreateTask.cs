1ef9aa64dba5b2db025752b9134c5dbb3071219f
KienLoveNH <kientri098@gmail.com>
2024-04-03 09:47:41 -0700

    Tạo task và thiết kế lại task

--- WFThesisManagementSystem/Forms/TeacherViews/Views/FTeacherCreateTask.cs
+++ WFThesisManagementSystem/Forms/TeacherViews/Views/FTeacherCreateTask.cs
@@ -1,13 +1,21 @@
 ﻿using System;
+using System.Threading.Tasks;
 using System.Windows.Forms;
+using WFThesisManagementSystem.Models;
+using WFThesisManagementSystem.Services;
+using Task = WFThesisManagementSystem.Models.Task;
 
 namespace WFThesisManagementSystem.Forms.TeacherViews.Views
 {
     public partial class FTeacherCreateTask : Form
     {
-        public FTeacherCreateTask()
+        int GroupId;
+        TaskService _taskService;
+        public FTeacherCreateTask(int Id)
         {
             InitializeComponent();
+            GroupId = Id;
+            //_taskService = new TaskService(task);
             ucTeacherCreateTask1.btnClose.Click += Close;
             ucTeacherCreateTask1.btnSave.Click += Save;
         }
@@ -17,7 +25,14 @@ namespace WFThesisManagementSystem.Forms.TeacherViews.Views
         }
         private void Save(object sender, EventArgs e)
         {
-
+            Task task = new Task();
+            task.task_name = ucTeacherCreateTask1.txtTaskName.Text;
+            task.task_description = ucTeacherCreateTask1.txtTaskDescription.Text;
+            task.due_date = ucTeacherCreateTask1.dtpEndDate.Value;
+            task.group_id = GroupId;
+            _taskService = new TaskService(task);
+            _taskService.CreateTask();
+            this.Hide();
         }
 
     }
