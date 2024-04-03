28e6e6373d457362f89636fd338b6f1fb8cbcf35
giahuynguyen28 <giahuy902@gmail.com>
2024-04-03 23:04:19 +0700

    Add function check IsTaskDone

--- WFThesisManagementSystem/Repositories/SubTaskRepository.cs
+++ WFThesisManagementSystem/Repositories/SubTaskRepository.cs
@@ -10,9 +10,10 @@ namespace WFThesisManagementSystem.Repositories
     public class SubTaskRepository : IRepository<SubTask>
     {   
         private ThesisManagementContext _context;
-
+        private TaskRepository _taskRepository;
         public SubTaskRepository(ThesisManagementContext context)
         {
+            _taskRepository = new TaskRepository(context);
             _context = context;
         }
 
@@ -30,6 +31,11 @@ namespace WFThesisManagementSystem.Repositories
         {
             _context.SubTasks.Add(entity);
             _context.SaveChanges();
+            var subtaskList = GetAll().Where(s => s.task_id == entity.task_id);
+            if(IsTaskDone(subtaskList))
+            {
+                _taskRepository.GetById(entity.task_id.Value).submit_day = DateTime.Now;
+            }
         }
 
         public void Update(SubTask entity)
@@ -42,6 +48,18 @@ namespace WFThesisManagementSystem.Repositories
         {
             _context.SubTasks.Remove(entity);
             _context.SaveChanges();
+        }
+
+        public bool IsTaskDone(IQueryable<SubTask> subTasks)
+        {
+            foreach (var subTask in subTasks)
+            {
+                if (subTask.submit_day == null)
+                {
+                    return false; 
+                }
+            }
+            return true; 
         }
     }
 }
