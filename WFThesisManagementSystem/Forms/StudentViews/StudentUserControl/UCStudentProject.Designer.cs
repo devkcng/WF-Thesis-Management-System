63f37986534c2e58cc1a6920e469560835aceeb4
giahuynguyen28 <giahuy902@gmail.com>
2024-04-04 12:10:08 +0700

    design usercontrol UCStudentProject

--- WFThesisManagementSystem/Forms/StudentViews/StudentUserControl/UCStudentProject.Designer.cs
+++ WFThesisManagementSystem/Forms/StudentViews/StudentUserControl/UCStudentProject.Designer.cs
@@ -28,75 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label6 = new System.Windows.Forms.Label();
-            this.label12 = new System.Windows.Forms.Label();
-            this.lblProject = new System.Windows.Forms.Label();
-            this.flowLayoutPanelContainer = new System.Windows.Forms.FlowLayoutPanel();
+            this.guna2ShadowPanel1 = new Guna.UI2.WinForms.Guna2ShadowPanel();
+            this.lblTasks = new System.Windows.Forms.Label();
+            this.flpAllTopics = new System.Windows.Forms.FlowLayoutPanel();
+            this.guna2ShadowPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label6
+            // guna2ShadowPanel1
             // 
-            this.label6.Font = new System.Drawing.Font("Arial", 10F);
-            this.label6.ForeColor = System.Drawing.Color.Silver;
-            this.label6.Location = new System.Drawing.Point(564, 769);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(95, 25);
-            this.label6.TabIndex = 0;
-            this.label6.Text = "Previous";
+            this.guna2ShadowPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2ShadowPanel1.Controls.Add(this.lblTasks);
+            this.guna2ShadowPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.guna2ShadowPanel1.FillColor = System.Drawing.Color.White;
+            this.guna2ShadowPanel1.Location = new System.Drawing.Point(0, 0);
+            this.guna2ShadowPanel1.Name = "guna2ShadowPanel1";
+            this.guna2ShadowPanel1.ShadowColor = System.Drawing.Color.Black;
+            this.guna2ShadowPanel1.Size = new System.Drawing.Size(2228, 188);
+            this.guna2ShadowPanel1.TabIndex = 0;
             // 
-            // label12
+            // lblTasks
             // 
-            this.label12.Font = new System.Drawing.Font("Arial", 10F);
-            this.label12.ForeColor = System.Drawing.Color.Silver;
-            this.label12.Location = new System.Drawing.Point(915, 769);
-            this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(54, 25);
-            this.label12.TabIndex = 52;
-            this.label12.Text = "Next";
+            this.lblTasks.AutoSize = true;
+            this.lblTasks.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTasks.Location = new System.Drawing.Point(5, 59);
+            this.lblTasks.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblTasks.Name = "lblTasks";
+            this.lblTasks.Size = new System.Drawing.Size(336, 63);
+            this.lblTasks.TabIndex = 39;
+            this.lblTasks.Text = "Your Project";
             // 
-            // lblProject
+            // flpAllTopics
             // 
-            this.lblProject.AutoSize = true;
-            this.lblProject.Dock = System.Windows.Forms.DockStyle.Top;
-            this.lblProject.Font = new System.Drawing.Font(".VnBahamasBH", 15F, System.Drawing.FontStyle.Bold);
-            this.lblProject.Location = new System.Drawing.Point(0, 0);
-            this.lblProject.Name = "lblProject";
-            this.lblProject.Size = new System.Drawing.Size(147, 44);
-            this.lblProject.TabIndex = 54;
-            this.lblProject.Text = "Projects";
-            this.lblProject.Click += new System.EventHandler(this.label2_Click);
-            // 
-            // flowLayoutPanelContainer
-            // 
-            this.flowLayoutPanelContainer.AutoScroll = true;
-            this.flowLayoutPanelContainer.BackColor = System.Drawing.SystemColors.Control;
-            this.flowLayoutPanelContainer.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.flowLayoutPanelContainer.Location = new System.Drawing.Point(0, 69);
-            this.flowLayoutPanelContainer.Name = "flowLayoutPanelContainer";
-            this.flowLayoutPanelContainer.Size = new System.Drawing.Size(1253, 649);
-            this.flowLayoutPanelContainer.TabIndex = 53;
+            this.flpAllTopics.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flpAllTopics.Location = new System.Drawing.Point(0, 188);
+            this.flpAllTopics.Name = "flpAllTopics";
+            this.flpAllTopics.Padding = new System.Windows.Forms.Padding(350, 50, 0, 300);
+            this.flpAllTopics.Size = new System.Drawing.Size(2228, 1049);
+            this.flpAllTopics.TabIndex = 1;
             // 
             // UCStudentProject
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Control;
-            this.Controls.Add(this.lblProject);
-            this.Controls.Add(this.flowLayoutPanelContainer);
-            this.Controls.Add(this.label12);
-            this.Controls.Add(this.label6);
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Controls.Add(this.flpAllTopics);
+            this.Controls.Add(this.guna2ShadowPanel1);
             this.Name = "UCStudentProject";
-            this.Size = new System.Drawing.Size(1253, 718);
+            this.Size = new System.Drawing.Size(2228, 1237);
+            this.guna2ShadowPanel1.ResumeLayout(false);
+            this.guna2ShadowPanel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label12;
-        private System.Windows.Forms.Label lblProject;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelContainer;
+
+        private Guna.UI2.WinForms.Guna2ShadowPanel guna2ShadowPanel1;
+        public System.Windows.Forms.Label lblTasks;
+        public System.Windows.Forms.FlowLayoutPanel flpAllTopics;
     }
 }
