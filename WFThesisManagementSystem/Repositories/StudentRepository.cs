cd8dccbcb4aac9742e6a0bcb8a0d21992be8044a
giahuynguyen28 <giahuy902@gmail.com>
2024-04-04 12:11:52 +0700

    nothing changes

--- WFThesisManagementSystem/Repositories/StudentRepository.cs
+++ WFThesisManagementSystem/Repositories/StudentRepository.cs
@@ -21,6 +21,7 @@ namespace WFThesisManagementSystem.Repositories
         {
             return _context.Students.FirstOrDefault(s => s.student_id == id);
         }
+
         public IQueryable<Student> GetAllByGroupId(int id)
         {
             return _context.Students.Where(t => t.group_id == id);
