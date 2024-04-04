ae9ab54f90ff0244fb8209ffdc479b0ab247568f
giahuynguyen28 <giahuy902@gmail.com>
2024-04-04 10:06:20 +0700

    rewrite the code following the repository pattern

--- WFThesisManagementSystem/Forms/StudentViews/Views/FStudentTaskPopUp.cs
+++ WFThesisManagementSystem/Forms/StudentViews/Views/FStudentTaskPopUp.cs
@@ -4,40 +4,28 @@ using System.Windows.Forms;
 using WFThesisManagementSystem.Forms.StudentViews.StudentUserControl;
 using WFThesisManagementSystem.Models;
 using WFThesisManagementSystem.Repositories;
+using WFThesisManagementSystem.Services;
 
 namespace WFThesisManagementSystem.Forms.StudentViews.Views
 {
     public partial class FStudentTaskPopUp : Form
     {
-        SubTask subTask = new SubTask();
-        SubTaskRepository _subTaskRepository;
-        ThesisManagementContext _context;
-
-        private int _id;
-        public int Id
-        {
-            get { return _id; }
-            set { _id = value; }
-        }
-        public FStudentTaskPopUp()
-        {
-            InitializeComponent();
-            _context = new ThesisManagementContext();
-            _subTaskRepository = new SubTaskRepository(_context);
-        }
+            SubTaskService _subTaskService;
+            public FStudentTaskPopUp(int subtaskID)
+            {
+                InitializeComponent();
+                _subTaskService = new SubTaskService(subtaskID);
+            }
 
 
-        private void lblSubTaskName_Click(object sender, System.EventArgs e)
-        {
+            private void lblSubTaskName_Click(object sender, System.EventArgs e)
+            {
 
-        }
+            }
 
-        private void btnSubmit_Click(object sender, EventArgs e)
-        {
-            var subTask = _subTaskRepository.GetById(Id);
-            subTask.submit_day = DateTime.Now;
-            _subTaskRepository.Update(subTask);
-            MessageBox.Show("Added Successfully");
-        }
+            private void btnSubmit_Click(object sender, EventArgs e)
+            {
+                _subTaskService.SubmitSubTask();
+            }
     }
 }
