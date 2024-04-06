c317464b63890d33c2e74c45448908cf9ae481f3
KienLoveNH <kientri098@gmail.com>
2024-04-06 09:15:13 -0700

    Update Model Notification

--- WFThesisManagementSystem/Models/Model.Designer.cs
+++ WFThesisManagementSystem/Models/Model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'E:\Project Code\Winform\WF-Thesis-Management-System\WFThesisManagementSystem\Models\Model.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\DAI HOC DUNG HOC DAI\SECOND\WINDOWPROGRAMING\WF_Team\WFThesisManagementSystem\Models\Model.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
