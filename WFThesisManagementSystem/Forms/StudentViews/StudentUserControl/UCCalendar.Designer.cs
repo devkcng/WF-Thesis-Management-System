a2aa6b55b08e64d860cc6a9fa0834936f6ebb1c1
giahuynguyen28 <giahuy902@gmail.com>
2024-04-06 22:46:42 +0700

    create and design usercontrol calendar

---
+++ WFThesisManagementSystem/Forms/StudentViews/StudentUserControl/UCCalendar.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace WFThesisManagementSystem.Forms.StudentViews.StudentUserControl
+{
+    partial class UCCalendar
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UCCalendar));
+            this.flpDayContainer = new System.Windows.Forms.FlowLayoutPanel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblMY = new System.Windows.Forms.Label();
+            this.guna2CustomGradientPanel1 = new Guna.UI2.WinForms.Guna2CustomGradientPanel();
+            this.ptbPrevious = new System.Windows.Forms.PictureBox();
+            this.ptbNext = new System.Windows.Forms.PictureBox();
+            this.guna2CustomGradientPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbPrevious)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbNext)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flpDayContainer
+            // 
+            this.flpDayContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flpDayContainer.Location = new System.Drawing.Point(0, 140);
+            this.flpDayContainer.Name = "flpDayContainer";
+            this.flpDayContainer.Size = new System.Drawing.Size(1471, 658);
+            this.flpDayContainer.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 8.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(428, 91);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(164, 38);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Wednesday";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 8.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(635, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 38);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Thursday";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 8.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(235, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 38);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Tuesday";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 8.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(41, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 38);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Monday";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 8.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(1044, 91);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(129, 38);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Saturday";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.White;
+            this.label6.Font = new System.Drawing.Font("Segoe UI Semibold", 8.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(856, 91);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(94, 38);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Friday";
+            // 
+            // label7
+            // 
+            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.White;
+            this.label7.Font = new System.Drawing.Font("Segoe UI Semibold", 8.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(1240, 91);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(136, 38);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Weekend";
+            // 
+            // lblMY
+            // 
+            this.lblMY.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblMY.AutoSize = true;
+            this.lblMY.BackColor = System.Drawing.Color.White;
+            this.lblMY.Font = new System.Drawing.Font("Segoe UI Semibold", 15F, System.Drawing.FontStyle.Bold);
+            this.lblMY.Location = new System.Drawing.Point(3, 11);
+            this.lblMY.Name = "lblMY";
+            this.lblMY.Size = new System.Drawing.Size(292, 67);
+            this.lblMY.TabIndex = 15;
+            this.lblMY.Text = "Month Year";
+            // 
+            // guna2CustomGradientPanel1
+            // 
+            this.guna2CustomGradientPanel1.Controls.Add(this.ptbNext);
+            this.guna2CustomGradientPanel1.Controls.Add(this.ptbPrevious);
+            this.guna2CustomGradientPanel1.Controls.Add(this.lblMY);
+            this.guna2CustomGradientPanel1.Controls.Add(this.label7);
+            this.guna2CustomGradientPanel1.Controls.Add(this.label6);
+            this.guna2CustomGradientPanel1.Controls.Add(this.label5);
+            this.guna2CustomGradientPanel1.Controls.Add(this.label1);
+            this.guna2CustomGradientPanel1.Controls.Add(this.label2);
+            this.guna2CustomGradientPanel1.Controls.Add(this.label3);
+            this.guna2CustomGradientPanel1.Controls.Add(this.label4);
+            this.guna2CustomGradientPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.guna2CustomGradientPanel1.Location = new System.Drawing.Point(0, 0);
+            this.guna2CustomGradientPanel1.Name = "guna2CustomGradientPanel1";
+            this.guna2CustomGradientPanel1.Size = new System.Drawing.Size(1471, 140);
+            this.guna2CustomGradientPanel1.TabIndex = 7;
+            // 
+            // ptbPrevious
+            // 
+            this.ptbPrevious.BackColor = System.Drawing.Color.White;
+            this.ptbPrevious.Image = ((System.Drawing.Image)(resources.GetObject("ptbPrevious.Image")));
+            this.ptbPrevious.Location = new System.Drawing.Point(301, 23);
+            this.ptbPrevious.Name = "ptbPrevious";
+            this.ptbPrevious.Size = new System.Drawing.Size(53, 55);
+            this.ptbPrevious.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbPrevious.TabIndex = 16;
+            this.ptbPrevious.TabStop = false;
+            this.ptbPrevious.Click += new System.EventHandler(this.ptbPrevious_Click);
+            // 
+            // ptbNext
+            // 
+            this.ptbNext.BackColor = System.Drawing.Color.White;
+            this.ptbNext.Image = ((System.Drawing.Image)(resources.GetObject("ptbNext.Image")));
+            this.ptbNext.Location = new System.Drawing.Point(402, 23);
+            this.ptbNext.Name = "ptbNext";
+            this.ptbNext.Size = new System.Drawing.Size(53, 55);
+            this.ptbNext.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbNext.TabIndex = 17;
+            this.ptbNext.TabStop = false;
+            this.ptbNext.Click += new System.EventHandler(this.ptbNext_Click);
+            // 
+            // UCCalendar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.flpDayContainer);
+            this.Controls.Add(this.guna2CustomGradientPanel1);
+            this.Name = "UCCalendar";
+            this.Size = new System.Drawing.Size(1471, 798);
+            this.guna2CustomGradientPanel1.ResumeLayout(false);
+            this.guna2CustomGradientPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbPrevious)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbNext)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.FlowLayoutPanel flpDayContainer;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblMY;
+        private Guna.UI2.WinForms.Guna2CustomGradientPanel guna2CustomGradientPanel1;
+        private System.Windows.Forms.PictureBox ptbNext;
+        private System.Windows.Forms.PictureBox ptbPrevious;
+    }
+}
---
+++ WFThesisManagementSystem/Forms/StudentViews/StudentUserControl/UCCalendar.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFThesisManagementSystem.Forms.StudentViews.StudentUserControl
+{
+    public partial class UCCalendar : UserControl
+    {
+        int month, year;
+        public UCCalendar()
+        {
+            InitializeComponent();
+            DisplayDays();
+        }
+
+        private void ptbPrevious_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ptbNext_Click(object sender, EventArgs e)
+        {
+            flpDayContainer.Controls.Clear();
+            month++;
+            DateTime startOfTheMonth = new DateTime(year, month, 1);
+
+            int days = DateTime.DaysInMonth(year, month);
+
+            lblMY.Text = DateTimeFormatInfo.CurrentInfo.GetMonthName(month) + " " + year;
+            int dayoftheweek = Convert.ToInt32(startOfTheMonth.DayOfWeek.ToString("d")) + 1;
+
+            for (int i = 2; i < dayoftheweek; i++)
+            {
+                UCBlank uCBlank = new UCBlank();
+                flpDayContainer.Controls.Add(uCBlank);
+            }
+            for (int i = 1; i <= days; i++)
+            {
+                UCDay uCDay = new UCDay();
+                uCDay.days(i);
+               
+                flpDayContainer.Controls.Add(uCDay);
+            }
+        }
+
+        private void DisplayDays()
+        {
+            DateTime now = DateTime.Now;
+            month = now.Month;
+            year = now.Year;
+            DateTime startOfTheMonth = new DateTime(year, month, 1);
+
+            int days = DateTime.DaysInMonth(year, month);
+
+
+            lblMY.Text = DateTimeFormatInfo.CurrentInfo.GetMonthName(month) +" " +now.Year;
+            int dayoftheweek = Convert.ToInt32(startOfTheMonth.DayOfWeek.ToString("d"))+1;
+
+            for(int i=2; i < dayoftheweek; i++)
+            {
+                UCBlank uCBlank = new UCBlank();
+                flpDayContainer.Controls.Add(uCBlank);
+            }
+            for (int i = 1; i <= days; i++)
+            {
+                UCDay uCDay = new UCDay();
+                uCDay.days(i);
+                if (i == 1)
+                { 
+                    uCDay.BackColor = Color.LightGreen; 
+                    uCDay.lblDay.BackColor= Color.LightGreen;
+                }
+                if (i == 10)
+                {
+                    uCDay.BackColor = Color.LightYellow;
+                    uCDay.lblDay.BackColor = Color.LightYellow;
+                }
+                if (i == 30)
+                {
+                    uCDay.BackColor = Color.MediumVioletRed;
+                    uCDay.lblDay.BackColor = Color.MediumVioletRed;
+                }
+                flpDayContainer.Controls.Add(uCDay);
+
+            }
+        }
+
+
+
+    }
+}
