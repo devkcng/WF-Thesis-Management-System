3e4f7633934816a0926e1b08a70b1c4d790618ed
giahuynguyen28 <giahuy902@gmail.com>
2024-04-06 22:47:31 +0700

    create and design for usercontrol day

---
+++ WFThesisManagementSystem/Forms/StudentViews/StudentUserControl/UCDay.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace WFThesisManagementSystem.Forms.StudentViews.StudentUserControl
+{
+    partial class UCDay
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDay = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblDay
+            // 
+            this.lblDay.AutoSize = true;
+            this.lblDay.BackColor = System.Drawing.SystemColors.Control;
+            this.lblDay.Font = new System.Drawing.Font("Cascadia Mono SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDay.Location = new System.Drawing.Point(56, 32);
+            this.lblDay.Name = "lblDay";
+            this.lblDay.Size = new System.Drawing.Size(68, 52);
+            this.lblDay.TabIndex = 1;
+            this.lblDay.Text = "00";
+            // 
+            // UCDay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.Controls.Add(this.lblDay);
+            this.Name = "UCDay";
+            this.Size = new System.Drawing.Size(196, 126);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Label lblDay;
+    }
+}
