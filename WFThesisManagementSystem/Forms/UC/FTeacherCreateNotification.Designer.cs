afa5b72b553a98363ea5aeaf54e93d8ea21c34c0
KienLoveNH <kientri098@gmail.com>
2024-04-05 20:17:32 -0700

    Create FTeacher Notification

---
+++ WFThesisManagementSystem/Forms/UC/FTeacherCreateNotification.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace WFThesisManagementSystem.Forms.UC
+{
+    partial class FTeacherCreateNotification
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ucCreateNotification1 = new WFThesisManagementSystem.Forms.UC.UcCreateNotification();
+            this.SuspendLayout();
+            // 
+            // ucCreateNotification1
+            // 
+            this.ucCreateNotification1.BackColor = System.Drawing.Color.DarkKhaki;
+            this.ucCreateNotification1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ucCreateNotification1.Location = new System.Drawing.Point(0, 0);
+            this.ucCreateNotification1.Name = "ucCreateNotification1";
+            this.ucCreateNotification1.Size = new System.Drawing.Size(889, 770);
+            this.ucCreateNotification1.TabIndex = 0;
+            // 
+            // FTeacherCreateNotification
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(889, 770);
+            this.Controls.Add(this.ucCreateNotification1);
+            this.Name = "FTeacherCreateNotification";
+            this.Text = "FTeacherCreateNotification";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private UcCreateNotification ucCreateNotification1;
+    }
+}
---
+++ WFThesisManagementSystem/Forms/UC/FTeacherCreateNotification.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFThesisManagementSystem.Forms.UC
+{
+    public partial class FTeacherCreateNotification : Form
+    {
+        public FTeacherCreateNotification()
+        {
+            InitializeComponent();
+            ucCreateNotification1.btnClear.Click += Clear;
+            ucCreateNotification1.btnSave.Click += Save;
+            ucCreateNotification1.btnClose.Click += Close;
+        }
+        private void Clear(object sender, EventArgs e)
+        {
+            ucCreateNotification1.txtNotificationContent.Clear();
+            ucCreateNotification1.txtNotificationTitle.Clear();
+            ucCreateNotification1.cbxNotificationType.Items.Clear();
+        }
+        private void Save(object sender, EventArgs e) 
+        {
+            
+        }
+        private void Close(object sender, EventArgs e) 
+        {
+            this.Hide();
+        }
+    }
+}
