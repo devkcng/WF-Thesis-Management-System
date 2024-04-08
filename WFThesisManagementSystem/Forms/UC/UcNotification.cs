2a470edd19b1da7616d14a073e59dc065a6c4c7c
KienLoveNH <kientri098@gmail.com>
2024-04-08 21:06:11 +0700

    edit image and create properties UcNotification

--- WFThesisManagementSystem/Forms/UC/UcNotification.cs
+++ WFThesisManagementSystem/Forms/UC/UcNotification.cs
@@ -12,9 +12,48 @@ namespace WFThesisManagementSystem.Forms.UC
 {
     public partial class UcNotification : UserControl
     {
+        private event EventHandler CheckBox;
         public UcNotification()
         {
             InitializeComponent();
+            icmCheckbox.Click += IcmCheckbox_Click;
         }
+        #region Properties
+        private string name;
+        private string content;
+        private string sendBy;
+        private DateTime sendTime;
+        public string Name
+        {
+            set { name  = value; lblNotificationName.Text = value; }
+            get { return  name; }
+        }
+        public string Content
+        {
+            set { content = value; lblNotificationContent.Text = value; }
+            get { return content; }
+        }
+        public string SendBy
+        {
+            set { sendBy = value; lblSendBy.Text = value; }
+            get { return sendBy; }
+        }
+        public DateTime SendTime
+        {
+            set { sendTime = value; dtpSendTime.Value = value; }
+            get { return sendTime; }
+        }
+        #endregion
+        #region EventCheckBox
+        private void IcmCheckbox_Click(object sender, EventArgs e)
+        {
+            OnCheckBoxClicked(EventArgs.Empty);
+        }
+        protected virtual void OnCheckBoxClicked(EventArgs e)
+        {
+            CheckBox?.Invoke(this, EventArgs.Empty);
+        }
+
+        #endregion
     }
 }
--- WFThesisManagementSystem/Properties/Resources.Designer.cs
+++ WFThesisManagementSystem/Properties/Resources.Designer.cs
@@ -423,6 +423,16 @@ namespace WFThesisManagementSystem.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap notification1 {
+            get {
+                object obj = ResourceManager.GetObject("notification1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Ok {
             get {
                 object obj = ResourceManager.GetObject("Ok", resourceCulture);
