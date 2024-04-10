a3dc22afcfdc896c1daca3ee9a1aa815ee59e307
devkcng <kimmanhcuong230304@gmail.com>
2024-04-10 23:29:04 +0700

    refactor: reformat code

--- WFThesisManagementSystem/Forms/UC/FNotificationSendStudent.cs
+++ WFThesisManagementSystem/Forms/UC/FNotificationSendStudent.cs
@@ -40,6 +40,13 @@ namespace WFThesisManagementSystem.Forms.UC
             ucNotificationSendStudentAll1.btnBackToEdit.Click += Close;
             ucNotificationSendStudentAll1.btnSend.Click += Send;
         }
+
+        private void FNotificationSendStudent_Load(object sender, EventArgs e)
+        {
+            List_Load_Group();
+        }
+
+        #region InitUsercontrol
         private void List_Load_Group()
         {
             ucNotificationSendStudentAll1.flpNotificationGroup.Controls.Clear();
@@ -61,9 +68,54 @@ namespace WFThesisManagementSystem.Forms.UC
             }
 
         }
+        #endregion
+
+        #region OnclickedGroupEventHandler 
+
+        private void OnClicked_UcGroup(object sender, EventArgs e)
+        {
+            
+            List_Load_Student(sender, e);
+        }
+
+        private void List_Load_Student(object sender, EventArgs e)
+        {   
+            ucNotificationSendStudentAll1.flpNotificationStudent.Controls.Clear();
+            UcTeacherAcceptRegisterSingle ucTeacherAcceptRegisterSingle1 = sender as UcTeacherAcceptRegisterSingle;
+            var students = _studentRepository.GetAllByGroupId(ucTeacherAcceptRegisterSingle1.Id);
+            foreach (var student in students)
+            {
+                UcTeacherAcceptRegisterSingle ucTeacherAcceptRegisterSingle = new UcTeacherAcceptRegisterSingle();
+                ucTeacherAcceptRegisterSingle.NameStudent = student.student_name;
+                ucTeacherAcceptRegisterSingle.IdStudent = Convert.ToString(student.student_id);
+                ucTeacherAcceptRegisterSingle.Regist = "Select";
+                //if (student.group_id == ucTeacherAcceptRegisterSingle1.Id)
+                //{ 
+                //    ucTeacherAcceptRegisterSingle.cbxRegist.Checked = ucTeacherAcceptRegisterSingle1.cbxRegist.Checked;
+                //}
+
+                if (_students.Contains(student))
+                {
+                    ucTeacherAcceptRegisterSingle.cbxRegist.Checked = true;
+                }
+                else
+                {
+                    ucTeacherAcceptRegisterSingle.cbxRegist.Checked = false;
+                }
+                ucTeacherAcceptRegisterSingle.cbxRegist.Checked = ucTeacherAcceptRegisterSingle1.cbxRegist.Checked;
+                ucTeacherAcceptRegisterSingle.CheckClicked += PickStudent;
+                ucNotificationSendStudentAll1.flpNotificationStudent.Controls.Add(ucTeacherAcceptRegisterSingle);
+            }
+
+        }
+
+        #endregion
+
+        #region CheckboxGroupEventHandler
         private void Choose_Student(object sender, EventArgs e)
         {   
             Load_Student(sender);
+            PickStudentByGroupCheckbox(sender);
         }
         private void Load_Student(object sender)
         {   
@@ -92,35 +144,59 @@ namespace WFThesisManagementSystem.Forms.UC
                     }
                 }    
             }
-           
-
         }
 
-        private void OnClicked_UcGroup(object sender, EventArgs e)
+        //function to add all student in group to list student to send notification. dont add student already in list.
+        //this function when user click on group checkbox and dont need to load usercontrol student
+        private void PickStudentByGroupCheckbox(object sender)
         {
-            
-            List_Load_Student(sender, e);
-        }
-
-        private void List_Load_Student(object sender, EventArgs e)
-        {   
-            ucNotificationSendStudentAll1.flpNotificationStudent.Controls.Clear();
-            UcTeacherAcceptRegisterSingle ucTeacherAcceptRegisterSingle1 = sender as UcTeacherAcceptRegisterSingle;
-            var students = _studentRepository.GetAllByGroupId(ucTeacherAcceptRegisterSingle1.Id);
+            UcTeacherAcceptRegisterSingle ucTeacherAcceptRegisterSingle = sender as UcTeacherAcceptRegisterSingle;
+            var students = _studentRepository.GetAllByGroupId(ucTeacherAcceptRegisterSingle.Id);
             foreach (var student in students)
             {
-                    UcTeacherAcceptRegisterSingle ucTeacherAcceptRegisterSingle = new UcTeacherAcceptRegisterSingle();
-                    ucTeacherAcceptRegisterSingle.NameStudent = student.student_name;
-                    ucTeacherAcceptRegisterSingle.IdStudent = Convert.ToString(student.student_id);
-                    ucTeacherAcceptRegisterSingle.Regist = "Select";
-                    if (student.group_id == ucTeacherAcceptRegisterSingle1.Id)
-                    { 
-                        ucTeacherAcceptRegisterSingle.cbxRegist.Checked = ucTeacherAcceptRegisterSingle1.cbxRegist.Checked;
+                if (ucTeacherAcceptRegisterSingle.cbxRegist.Checked)
+                {
+                    if (!_students.Contains(student))
+                    {
+                        _students.Add(student);
                     }
-                    ucNotificationSendStudentAll1.flpNotificationStudent.Controls.Add(ucTeacherAcceptRegisterSingle);
+                }
+                else
+                {
+                    if (_students.Contains(student))
+                    {
+                        _students.Remove(student);
+                    }
+                }
             }
-
         }
+
+        #endregion
+
+        #region CheckboxStudentEventHandler
+
+        void PickStudent(object sender, EventArgs e)
+        {
+            UcTeacherAcceptRegisterSingle ucTeacherAcceptRegisterSingle = sender as UcTeacherAcceptRegisterSingle;
+            var student = _studentRepository.GetById(int.Parse(ucTeacherAcceptRegisterSingle.IdStudent));
+            if (ucTeacherAcceptRegisterSingle.cbxRegist.Checked)
+            {
+                if (!_students.Contains(student))
+                {
+                    _students.Add(student);
+                }
+            }
+            else
+            {
+                if (_students.Contains(student))
+                {
+                    _students.Remove(student);
+                }
+            }
+        }
+
+        #endregion
+
         private void Close(object sender, EventArgs e)
         {
             this.Close();
@@ -136,11 +212,6 @@ namespace WFThesisManagementSystem.Forms.UC
             }
             else MessageBox.Show("Send To Student Failture");
 
-        }
-
-        private void FNotificationSendStudent_Load(object sender, EventArgs e)
-        {
-            List_Load_Group();
         }
     }
 }
