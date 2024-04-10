3583eb1e5006ab75322e23c9fafb18d8aa46d245
devkcng <kimmanhcuong230304@gmail.com>
2024-04-10 22:50:37 +0700

    refactor: delete unuse object, move logic to service class

--- WFThesisManagementSystem/Forms/UC/UcNotificationSendStudentAll.Designer.cs
+++ WFThesisManagementSystem/Forms/UC/UcNotificationSendStudentAll.Designer.cs
@@ -29,13 +29,11 @@
         private void InitializeComponent()
         {
             this.flpNotificationStudent = new System.Windows.Forms.FlowLayoutPanel();
-            this.ucTeacherAcceptRegisterSingle2 = new WFThesisManagementSystem.Forms.TeacherViews.TeacherUserControl.UcTeacherAcceptRegisterSingle();
             this.lblNotificationStudent = new System.Windows.Forms.Label();
             this.ucTeacherAcceptRegisterSingle1 = new WFThesisManagementSystem.Forms.TeacherViews.TeacherUserControl.UcTeacherAcceptRegisterSingle();
             this.flpNotificationGroup = new System.Windows.Forms.FlowLayoutPanel();
             this.btnBackToEdit = new Guna.UI2.WinForms.Guna2Button();
             this.btnSend = new Guna.UI2.WinForms.Guna2Button();
-            this.flpNotificationStudent.SuspendLayout();
             this.flpNotificationGroup.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -43,31 +41,20 @@
             // 
             this.flpNotificationStudent.AutoScroll = true;
             this.flpNotificationStudent.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.flpNotificationStudent.Controls.Add(this.ucTeacherAcceptRegisterSingle2);
-            this.flpNotificationStudent.Location = new System.Drawing.Point(879, 158);
+            this.flpNotificationStudent.Location = new System.Drawing.Point(586, 103);
+            this.flpNotificationStudent.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.flpNotificationStudent.Name = "flpNotificationStudent";
-            this.flpNotificationStudent.Size = new System.Drawing.Size(626, 725);
+            this.flpNotificationStudent.Size = new System.Drawing.Size(419, 473);
             this.flpNotificationStudent.TabIndex = 15;
-            // 
-            // ucTeacherAcceptRegisterSingle2
-            // 
-            this.ucTeacherAcceptRegisterSingle2.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.ucTeacherAcceptRegisterSingle2.CheckRegist = null;
-            this.ucTeacherAcceptRegisterSingle2.IdStudent = null;
-            this.ucTeacherAcceptRegisterSingle2.Location = new System.Drawing.Point(3, 3);
-            this.ucTeacherAcceptRegisterSingle2.Name = "ucTeacherAcceptRegisterSingle2";
-            this.ucTeacherAcceptRegisterSingle2.NameStudent = null;
-            this.ucTeacherAcceptRegisterSingle2.Regist = null;
-            this.ucTeacherAcceptRegisterSingle2.Size = new System.Drawing.Size(618, 150);
-            this.ucTeacherAcceptRegisterSingle2.TabIndex = 0;
             // 
             // lblNotificationStudent
             // 
             this.lblNotificationStudent.AutoSize = true;
             this.lblNotificationStudent.Font = new System.Drawing.Font("Century Gothic", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNotificationStudent.Location = new System.Drawing.Point(101, 92);
+            this.lblNotificationStudent.Location = new System.Drawing.Point(67, 60);
+            this.lblNotificationStudent.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblNotificationStudent.Name = "lblNotificationStudent";
-            this.lblNotificationStudent.Size = new System.Drawing.Size(261, 38);
+            this.lblNotificationStudent.Size = new System.Drawing.Size(180, 26);
             this.lblNotificationStudent.TabIndex = 14;
             this.lblNotificationStudent.Text = "Send To Student";
             // 
@@ -75,12 +62,14 @@
             // 
             this.ucTeacherAcceptRegisterSingle1.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.ucTeacherAcceptRegisterSingle1.CheckRegist = null;
+            this.ucTeacherAcceptRegisterSingle1.Id = 0;
             this.ucTeacherAcceptRegisterSingle1.IdStudent = null;
-            this.ucTeacherAcceptRegisterSingle1.Location = new System.Drawing.Point(3, 3);
+            this.ucTeacherAcceptRegisterSingle1.Location = new System.Drawing.Point(1, 1);
+            this.ucTeacherAcceptRegisterSingle1.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.ucTeacherAcceptRegisterSingle1.Name = "ucTeacherAcceptRegisterSingle1";
             this.ucTeacherAcceptRegisterSingle1.NameStudent = null;
             this.ucTeacherAcceptRegisterSingle1.Regist = null;
-            this.ucTeacherAcceptRegisterSingle1.Size = new System.Drawing.Size(621, 150);
+            this.ucTeacherAcceptRegisterSingle1.Size = new System.Drawing.Size(414, 98);
             this.ucTeacherAcceptRegisterSingle1.TabIndex = 0;
             // 
             // flpNotificationGroup
@@ -88,9 +77,10 @@
             this.flpNotificationGroup.AutoScroll = true;
             this.flpNotificationGroup.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.flpNotificationGroup.Controls.Add(this.ucTeacherAcceptRegisterSingle1);
-            this.flpNotificationGroup.Location = new System.Drawing.Point(79, 153);
+            this.flpNotificationGroup.Location = new System.Drawing.Point(53, 99);
+            this.flpNotificationGroup.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.flpNotificationGroup.Name = "flpNotificationGroup";
-            this.flpNotificationGroup.Size = new System.Drawing.Size(634, 730);
+            this.flpNotificationGroup.Size = new System.Drawing.Size(424, 476);
             this.flpNotificationGroup.TabIndex = 13;
             // 
             // btnBackToEdit
@@ -103,9 +93,10 @@
             this.btnBackToEdit.FillColor = System.Drawing.Color.WhiteSmoke;
             this.btnBackToEdit.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnBackToEdit.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.btnBackToEdit.Location = new System.Drawing.Point(1286, 900);
+            this.btnBackToEdit.Location = new System.Drawing.Point(857, 585);
+            this.btnBackToEdit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnBackToEdit.Name = "btnBackToEdit";
-            this.btnBackToEdit.Size = new System.Drawing.Size(170, 55);
+            this.btnBackToEdit.Size = new System.Drawing.Size(113, 36);
             this.btnBackToEdit.TabIndex = 17;
             this.btnBackToEdit.Text = "Back to edit";
             // 
@@ -118,24 +109,25 @@
             this.btnSend.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
             this.btnSend.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSend.ForeColor = System.Drawing.Color.White;
-            this.btnSend.Location = new System.Drawing.Point(1143, 900);
+            this.btnSend.Location = new System.Drawing.Point(762, 585);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSend.Name = "btnSend";
-            this.btnSend.Size = new System.Drawing.Size(104, 55);
+            this.btnSend.Size = new System.Drawing.Size(69, 36);
             this.btnSend.TabIndex = 16;
             this.btnSend.Text = "Send";
             // 
             // UcNotificationSendStudentAll
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.btnBackToEdit);
             this.Controls.Add(this.btnSend);
             this.Controls.Add(this.flpNotificationStudent);
             this.Controls.Add(this.lblNotificationStudent);
             this.Controls.Add(this.flpNotificationGroup);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "UcNotificationSendStudentAll";
-            this.Size = new System.Drawing.Size(1584, 974);
-            this.flpNotificationStudent.ResumeLayout(false);
+            this.Size = new System.Drawing.Size(1056, 633);
             this.flpNotificationGroup.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -145,7 +137,6 @@
         #endregion
 
         public System.Windows.Forms.FlowLayoutPanel flpNotificationStudent;
-        public TeacherViews.TeacherUserControl.UcTeacherAcceptRegisterSingle ucTeacherAcceptRegisterSingle2;
         public System.Windows.Forms.Label lblNotificationStudent;
         public TeacherViews.TeacherUserControl.UcTeacherAcceptRegisterSingle ucTeacherAcceptRegisterSingle1;
         public System.Windows.Forms.FlowLayoutPanel flpNotificationGroup;
