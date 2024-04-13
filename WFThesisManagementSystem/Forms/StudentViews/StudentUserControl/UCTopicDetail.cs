47b93db6565e524cfb7e0a15a5a14f98af8083b4
giahuynguyen28 <giahuy902@gmail.com>
2024-04-14 00:04:22 +0700

    create a usercontrol to display information of a topic

---
+++ WFThesisManagementSystem/Forms/StudentViews/StudentUserControl/UCTopicDetail.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFThesisManagementSystem.Forms.StudentViews.StudentUserControl
+{
+    public partial class UCTopicDetail : UserControl
+    {
+        public UCTopicDetail()
+        {
+            InitializeComponent();
+            // Tính toán chiều cao cần thiết để hiển thị toàn bộ văn bản
+            int newHeight = TextRenderer.MeasureText(txtDescription.Text, txtDescription.Font, txtDescription.ClientSize, TextFormatFlags.WordBreak).Height;
+
+            // Đặt chiều cao mới cho TextBox
+            txtDescription.Height = newHeight;
+        }
+        private void txtDescrssiption_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtDescription_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
