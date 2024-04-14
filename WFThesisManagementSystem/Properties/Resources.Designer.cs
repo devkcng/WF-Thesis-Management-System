fb5224f1c834a54175f3afc820b3809f6a184bf7
giahuynguyen28 <giahuy902@gmail.com>
2024-04-14 12:44:41 +0700

    add the calendar picture

--- WFThesisManagementSystem/Properties/Resources.Designer.cs
+++ WFThesisManagementSystem/Properties/Resources.Designer.cs
@@ -343,6 +343,16 @@ namespace WFThesisManagementSystem.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap icons8_calendar_48 {
+            get {
+                object obj = ResourceManager.GetObject("icons8-calendar-48", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap icons8_chevron_left_64 {
             get {
                 object obj = ResourceManager.GetObject("icons8-chevron-left-64", resourceCulture);
