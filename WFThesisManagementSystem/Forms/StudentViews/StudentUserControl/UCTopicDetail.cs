1e1d44f5a8badff7f550089678e47c24198dd6e0
giahuynguyen28 <giahuy902@gmail.com>
2024-04-21 16:37:46 +0700

    add function change color for component in usercontrol

--- WFThesisManagementSystem/Forms/StudentViews/StudentUserControl/UCTopicDetail.cs
+++ WFThesisManagementSystem/Forms/StudentViews/StudentUserControl/UCTopicDetail.cs
@@ -15,20 +15,27 @@ namespace WFThesisManagementSystem.Forms.StudentViews.StudentUserControl
         public UCTopicDetail()
         {
             InitializeComponent();
-            // Tính toán chiều cao cần thiết để hiển thị toàn bộ văn bản
-            int newHeight = TextRenderer.MeasureText(txtDescription.Text, txtDescription.Font, txtDescription.ClientSize, TextFormatFlags.WordBreak).Height;
+            //// Tính toán chiều cao cần thiết để hiển thị toàn bộ văn bản
+            //int newHeight = TextRenderer.MeasureText(txtDescription.Text, txtDescription.Font, txtDescription.ClientSize, TextFormatFlags.WordBreak).Height;
 
-            // Đặt chiều cao mới cho TextBox
-            txtDescription.Height = newHeight;
+            //// Đặt chiều cao mới cho TextBox
+            //txtDescription.Height = newHeight;
         }
-        private void txtDescrssiption_TextChanged(object sender, EventArgs e)
+        public void SetColors(string textColorHex, string backColorHex)
         {
+            Color textColor = ColorTranslator.FromHtml(textColorHex); // Chuyển đổi mã hex sang màu
+            Color backColor = ColorTranslator.FromHtml(backColorHex); // Chuyển đổi mã hex sang màu
 
-        }
+            // Đặt màu nền cho Panel bên trong
+            foreach (Control ctrl in panelContainer.Controls)
+            {
+                ctrl.BackColor = backColor;
+                ctrl.ForeColor = textColor;
+            }
 
-        private void txtDescription_TextChanged(object sender, EventArgs e)
-        {
-
+            // Đặt màu nền cho UserControl
+            this.BackColor = backColor;
+            panelContainer.BackColor=backColor;
         }
     }
 }
