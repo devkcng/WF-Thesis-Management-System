a08c8e062afc18ce7deeb536f10cd633f784d2e6
giahuynguyen28 <giahuy902@gmail.com>
2024-04-24 11:06:09 +0700

    design UI again for UcTeacherSingleTopic

--- WFThesisManagementSystem/Forms/TeacherViews/TeacherUserControl/UcTeacherSingleTopic.Designer.cs
+++ WFThesisManagementSystem/Forms/TeacherViews/TeacherUserControl/UcTeacherSingleTopic.Designer.cs
@@ -29,57 +29,57 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UcTeacherSingleTopic));
-            this.lblDescription = new System.Windows.Forms.Label();
-            this.lblLine = new System.Windows.Forms.Label();
-            this.plName = new System.Windows.Forms.Panel();
+            this.guna2CustomGradientPanel1 = new Guna.UI2.WinForms.Guna2CustomGradientPanel();
             this.lblName = new System.Windows.Forms.Label();
+            this.lblDescription = new System.Windows.Forms.Label();
             this.btnEdit = new Guna.UI2.WinForms.Guna2Button();
-            this.guna2ShadowPanel1 = new Guna.UI2.WinForms.Guna2ShadowPanel();
-            this.plName.SuspendLayout();
-            this.guna2ShadowPanel1.SuspendLayout();
+            this.lblLine = new System.Windows.Forms.Label();
+            this.guna2CustomGradientPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // lblDescription
+            // guna2CustomGradientPanel1
             // 
-            this.lblDescription.AutoSize = true;
-            this.lblDescription.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDescription.Location = new System.Drawing.Point(24, 157);
-            this.lblDescription.Name = "lblDescription";
-            this.lblDescription.Size = new System.Drawing.Size(147, 30);
-            this.lblDescription.TabIndex = 3;
-            this.lblDescription.Text = "Description";
-            // 
-            // lblLine
-            // 
-            this.lblLine.AutoSize = true;
-            this.lblLine.Location = new System.Drawing.Point(25, 117);
-            this.lblLine.Name = "lblLine";
-            this.lblLine.Size = new System.Drawing.Size(1094, 20);
-            this.lblLine.TabIndex = 5;
-            this.lblLine.Text = resources.GetString("lblLine.Text");
-            // 
-            // plName
-            // 
-            this.plName.BackColor = System.Drawing.Color.LightBlue;
-            this.plName.Controls.Add(this.lblName);
-            this.plName.Location = new System.Drawing.Point(29, 24);
-            this.plName.Name = "plName";
-            this.plName.Size = new System.Drawing.Size(982, 73);
-            this.plName.TabIndex = 6;
+            this.guna2CustomGradientPanel1.BorderColor = System.Drawing.Color.Black;
+            this.guna2CustomGradientPanel1.BorderRadius = 10;
+            this.guna2CustomGradientPanel1.BorderThickness = 2;
+            this.guna2CustomGradientPanel1.Controls.Add(this.lblName);
+            this.guna2CustomGradientPanel1.Controls.Add(this.lblDescription);
+            this.guna2CustomGradientPanel1.Controls.Add(this.btnEdit);
+            this.guna2CustomGradientPanel1.Controls.Add(this.lblLine);
+            this.guna2CustomGradientPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.guna2CustomGradientPanel1.Location = new System.Drawing.Point(20, 20);
+            this.guna2CustomGradientPanel1.Name = "guna2CustomGradientPanel1";
+            this.guna2CustomGradientPanel1.Size = new System.Drawing.Size(2276, 315);
+            this.guna2CustomGradientPanel1.TabIndex = 0;
             // 
             // lblName
             // 
             this.lblName.AutoSize = true;
-            this.lblName.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblName.Location = new System.Drawing.Point(12, 18);
+            this.lblName.BackColor = System.Drawing.Color.White;
+            this.lblName.Font = new System.Drawing.Font("Cambria", 15.9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.Color.Black;
+            this.lblName.Location = new System.Drawing.Point(77, 20);
+            this.lblName.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(87, 30);
-            this.lblName.TabIndex = 7;
+            this.lblName.Size = new System.Drawing.Size(166, 62);
+            this.lblName.TabIndex = 10;
             this.lblName.Text = "Name";
             this.lblName.Click += new System.EventHandler(this.lblName_Click);
             // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescription.Location = new System.Drawing.Point(80, 138);
+            this.lblDescription.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(236, 47);
+            this.lblDescription.TabIndex = 8;
+            this.lblDescription.Text = "Description";
+            // 
             // btnEdit
             // 
+            this.btnEdit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btnEdit.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
             this.btnEdit.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
             this.btnEdit.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
@@ -89,50 +89,48 @@
             this.btnEdit.ForeColor = System.Drawing.Color.White;
             this.btnEdit.Image = global::WFThesisManagementSystem.Properties.Resources.Create;
             this.btnEdit.ImageSize = new System.Drawing.Size(40, 40);
-            this.btnEdit.Location = new System.Drawing.Point(1066, 42);
+            this.btnEdit.Location = new System.Drawing.Point(2158, 9);
+            this.btnEdit.Margin = new System.Windows.Forms.Padding(5);
             this.btnEdit.Name = "btnEdit";
-            this.btnEdit.Size = new System.Drawing.Size(53, 48);
-            this.btnEdit.TabIndex = 1;
+            this.btnEdit.Size = new System.Drawing.Size(94, 74);
+            this.btnEdit.TabIndex = 7;
             this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
             // 
-            // guna2ShadowPanel1
+            // lblLine
             // 
-            this.guna2ShadowPanel1.BackColor = System.Drawing.Color.Transparent;
-            this.guna2ShadowPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.guna2ShadowPanel1.Controls.Add(this.lblDescription);
-            this.guna2ShadowPanel1.Controls.Add(this.btnEdit);
-            this.guna2ShadowPanel1.Controls.Add(this.plName);
-            this.guna2ShadowPanel1.Controls.Add(this.lblLine);
-            this.guna2ShadowPanel1.FillColor = System.Drawing.Color.White;
-            this.guna2ShadowPanel1.Location = new System.Drawing.Point(0, 0);
-            this.guna2ShadowPanel1.Name = "guna2ShadowPanel1";
-            this.guna2ShadowPanel1.ShadowColor = System.Drawing.Color.Black;
-            this.guna2ShadowPanel1.Size = new System.Drawing.Size(1178, 229);
-            this.guna2ShadowPanel1.TabIndex = 7;
+            this.lblLine.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblLine.AutoSize = true;
+            this.lblLine.Location = new System.Drawing.Point(49, 83);
+            this.lblLine.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblLine.Name = "lblLine";
+            this.lblLine.Size = new System.Drawing.Size(2192, 32);
+            this.lblLine.TabIndex = 9;
+            this.lblLine.Text = resources.GetString("lblLine.Text");
             // 
             // UcTeacherSingleTopic
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.Controls.Add(this.guna2ShadowPanel1);
+            this.Controls.Add(this.guna2CustomGradientPanel1);
+            this.Margin = new System.Windows.Forms.Padding(5);
             this.Name = "UcTeacherSingleTopic";
-            this.Size = new System.Drawing.Size(1178, 229);
+            this.Padding = new System.Windows.Forms.Padding(20);
+            this.Size = new System.Drawing.Size(2316, 355);
             this.Load += new System.EventHandler(this.UcTeacherSingleTopic_Load);
-            this.plName.ResumeLayout(false);
-            this.plName.PerformLayout();
-            this.guna2ShadowPanel1.ResumeLayout(false);
-            this.guna2ShadowPanel1.PerformLayout();
+            this.guna2CustomGradientPanel1.ResumeLayout(false);
+            this.guna2CustomGradientPanel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-        public Guna.UI2.WinForms.Guna2Button btnEdit;
+
+        private Guna.UI2.WinForms.Guna2CustomGradientPanel guna2CustomGradientPanel1;
         public System.Windows.Forms.Label lblDescription;
+        public Guna.UI2.WinForms.Guna2Button btnEdit;
         public System.Windows.Forms.Label lblLine;
-        private System.Windows.Forms.Panel plName;
         public System.Windows.Forms.Label lblName;
-        private Guna.UI2.WinForms.Guna2ShadowPanel guna2ShadowPanel1;
     }
 }
