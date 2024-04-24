93f6300f57ebe4e52434fb00fd8bfd1eb3599ebe
giahuynguyen28 <giahuy902@gmail.com>
2024-04-24 19:10:47 +0700

    delete redundant code

--- WFThesisManagementSystem/Forms/StudentViews/StudentUserControl/UCTopicDetail.cs
+++ WFThesisManagementSystem/Forms/StudentViews/StudentUserControl/UCTopicDetail.cs
@@ -16,19 +16,6 @@ namespace WFThesisManagementSystem.Forms.StudentViews.StudentUserControl
         public UCTopicDetail()
         {
             InitializeComponent();
-            //// Tính toán chiều cao cần thiết để hiển thị toàn bộ văn bản
-            //int newHeight = TextRenderer.MeasureText(txtDescription.Text, txtDescription.Font, txtDescription.ClientSize, TextFormatFlags.WordBreak).Height;
-
-            //// Đặt chiều cao mới cho TextBox
-            //txtDescription.Height = newHeight;
-            // Điều chỉnh width của các TextBox khi nội dung thay đổi
-            //txtTeacherName.TextChanged += (s, ev) => AdjustTextBoxWidth(txtTeacherName, 1500);
-            //txtCategory.TextChanged += (s, ev) => AdjustTextBoxWidth(txtCategory, 1500);
-
-            //// Điều chỉnh width của các TextBox ban đầu
-            //AdjustTextBoxWidth(txtTeacherName,1500);
-            //AdjustTextBoxWidth(txtCategory, 1500);
-
         }
         public void SetColors(string textColorHex, string backColorHex)
         {
@@ -45,27 +32,6 @@ namespace WFThesisManagementSystem.Forms.StudentViews.StudentUserControl
             // Đặt màu nền cho UserControl
             this.BackColor = backColor;
             panelContainer.BackColor=backColor;
-        }
-
-        private void AdjustTextBoxWidth(Guna2TextBox textBox, int maxWidth)
-        {
-            int textWidth = TextRenderer.MeasureText(textBox.Text, textBox.Font).Width+10;
-            textBox.Width = Math.Min(textWidth, maxWidth);
-        }
-
-        private void panelContainer_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void txtTechnology_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void lblTechnology_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
