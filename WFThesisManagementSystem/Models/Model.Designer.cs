23da398beb1348631c7aa4179515846bc9a2a938
giahuynguyen28 <giahuy902@gmail.com>
2024-04-25 14:23:05 +0700

    add new table to database

--- WFThesisManagementSystem/Models/Model.Designer.cs
+++ WFThesisManagementSystem/Models/Model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\DAI HOC DUNG HOC DAI\SECOND\WINDOWPROGRAMING\WF_Team\WFThesisManagementSystem\Models\Model.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Documents\School\WF-Thesis-Management-System\WFThesisManagementSystem\Models\Model.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
