dcf7ff59cb393cbd4345e0245f2a3173526cfae2
giahuynguyen28 <giahuy902@gmail.com>
2024-04-25 15:19:57 +0700

    add fucntion return list 5 teacher has most student regist

--- WFThesisManagementSystem/Services/TeacherService.cs
+++ WFThesisManagementSystem/Services/TeacherService.cs
@@ -24,7 +24,7 @@ namespace WFThesisManagementSystem.Services
             _topicRepository = new TopicRepository(_context);
             _studentGroupRepository = new StudentGroupRepository(_context);
             _teacherRepository = new TeacherRepository(_context);
-            _teacher= new Teacher();
+            _teacher = new Teacher();
             _teacher = _teacherRepository.GetById(teacherID);
         }
         public int GetNumberStudentRegisterOfTeacher()
@@ -43,6 +43,43 @@ namespace WFThesisManagementSystem.Services
                 }
             }
             return count;
+        }
+        private int GetNumberStudentRegisterOfTeacher(int teacher_id)
+        {
+            int count = 0;
+            var topicList = _topicRepository.GetAllByTeacherID(teacher_id);
+            foreach (var topic in topicList)
+            {
+                var studentGroupList = _studentGroupRepository.GetAllByTopicID(topic.topic_id);
+                foreach (var studentGroup in studentGroupList)
+                {
+                    if (studentGroup != null)
+                    {
+                        count += studentGroup.number_of_students.Value;
+                    }
+                }
+            }
+            return count;
+        }
+
+        public List<Teacher> GetTeachersHasMostStudentRegist() 
+        {
+            var teacherList = _teacherRepository.GetAll();
+            List<(int teacher_id, int numberStudentRegist)> teacherOrderingList = new List<(int, int)>();
+            foreach(var teacher in teacherList)
+            {
+                teacherOrderingList.Add((teacher.teacher_id, GetNumberStudentRegisterOfTeacher(teacher.teacher_id)));
+            }
+            teacherOrderingList = teacherOrderingList.OrderByDescending(t => t.numberStudentRegist).ToList();
+            teacherOrderingList = teacherOrderingList.Take(5).ToList();
+            // Tạo danh sách giáo viên từ danh sách đã sắp xếp
+            List<Teacher> teachers = new List<Teacher>();
+            foreach (var tuple in teacherOrderingList)
+            {
+                Teacher teacher = _teacherRepository.GetById(tuple.teacher_id);
+                teachers.Add(teacher);
+            }
+            return teachers;
         }
     }
 }
