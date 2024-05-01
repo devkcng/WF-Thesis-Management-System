a4d5dfaec33dc183672ea56e5601772130b5f884
giahuynguyen28 <giahuy902@gmail.com>
2024-05-01 18:40:05 +0700

    add event for the assign task button and the backpage button

--- WFThesisManagementSystem/Forms/StudentViews/StudentUserControl/UCStudentSubTasks.cs
+++ WFThesisManagementSystem/Forms/StudentViews/StudentUserControl/UCStudentSubTasks.cs
@@ -12,10 +12,30 @@ namespace WFThesisManagementSystem.Forms.StudentViews.StudentUserControl
 {
     public partial class UCSTudentSubTasks : UserControl
     {
+        public event EventHandler previousPtbClicked;
+        public event EventHandler<int> AssignTaskBtnClicked;
+        public int _taskID;
         public UCSTudentSubTasks()
         {
             InitializeComponent();
+            this.btnPrevious.Click += btnPrevious_Click;
+        }
 
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            OnPreviousPtbClicked(EventArgs.Empty);
+        }
+        protected virtual void OnPreviousPtbClicked(EventArgs e)
+        {
+            previousPtbClicked?.Invoke(this, e);
+        }
+
+        private void btnAssignTask_Click(object sender, EventArgs e)
+        {
+                if (AssignTaskBtnClicked != null)
+                {
+                    AssignTaskBtnClicked(this, _taskID);
+                }
         }
     }
 }
