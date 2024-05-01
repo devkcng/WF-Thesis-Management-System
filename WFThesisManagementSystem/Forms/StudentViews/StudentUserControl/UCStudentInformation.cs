a3d96c650f514d53b7c5b8de3397dd4b9b854fb8
giahuynguyen28 <giahuy902@gmail.com>
2024-05-01 18:38:59 +0700

    add event when clicking on the ucstudentinformation, the flpsubtask will list all the subtask of that student

--- WFThesisManagementSystem/Forms/StudentViews/StudentUserControl/UCStudentInformation.cs
+++ WFThesisManagementSystem/Forms/StudentViews/StudentUserControl/UCStudentInformation.cs
@@ -12,9 +12,23 @@ namespace WFThesisManagementSystem.Forms.StudentViews.StudentUserControl
 {
     public partial class UCStudentInformation : UserControl
     {
+        public event EventHandler Clicked;
+        public int _id;
+        public int _taskID;
+
         public UCStudentInformation()
         {
             InitializeComponent();
+            panelContainer.Click += UCStudentInformation_CLick;
         }
+        protected virtual void OnClicked(EventArgs e)
+        {
+            Clicked?.Invoke(this, e);
+        }
+        public void UCStudentInformation_CLick(object sender, EventArgs e)
+        {
+            OnClicked(EventArgs.Empty);
+        }
+
     }
 }
