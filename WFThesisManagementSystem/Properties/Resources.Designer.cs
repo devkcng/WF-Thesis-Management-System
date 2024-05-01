6b7b43f02eb839d397e90fc11830f96a0bc5cdd0
giahuynguyen28 <giahuy902@gmail.com>
2024-05-01 18:46:02 +0700

    add picture to resources

--- WFThesisManagementSystem/Properties/Resources.Designer.cs
+++ WFThesisManagementSystem/Properties/Resources.Designer.cs
@@ -343,6 +343,16 @@ namespace WFThesisManagementSystem.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap icons8_back_24 {
+            get {
+                object obj = ResourceManager.GetObject("icons8-back-24", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap icons8_calendar_48 {
             get {
                 object obj = ResourceManager.GetObject("icons8-calendar-48", resourceCulture);
