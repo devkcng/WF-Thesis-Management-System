0c11f504be03b2cefe24f6f37f66badc8eedd406
KienLoveNH <kientri098@gmail.com>
2024-05-01 22:47:35 +0700

    Create Fteacher mark

---
+++ WFThesisManagementSystem/Forms/TeacherViews/Views/FTeacherMark.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace WFThesisManagementSystem.Forms.TeacherViews.Views
+{
+    partial class FTeacherMark
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.guna2CustomGradientPanel1 = new Guna.UI2.WinForms.Guna2CustomGradientPanel();
+            this.ucTeacherAllMark1 = new WFThesisManagementSystem.Forms.TeacherViews.TeacherUserControl.UcTeacherAllMark();
+            this.guna2CustomGradientPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // guna2CustomGradientPanel1
+            // 
+            this.guna2CustomGradientPanel1.Controls.Add(this.ucTeacherAllMark1);
+            this.guna2CustomGradientPanel1.Location = new System.Drawing.Point(12, 12);
+            this.guna2CustomGradientPanel1.Name = "guna2CustomGradientPanel1";
+            this.guna2CustomGradientPanel1.Size = new System.Drawing.Size(1900, 1026);
+            this.guna2CustomGradientPanel1.TabIndex = 0;
+            // 
+            // ucTeacherAllMark1
+            // 
+            this.ucTeacherAllMark1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ucTeacherAllMark1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ucTeacherAllMark1.Location = new System.Drawing.Point(0, 0);
+            this.ucTeacherAllMark1.Name = "ucTeacherAllMark1";
+            this.ucTeacherAllMark1.Size = new System.Drawing.Size(1900, 1026);
+            this.ucTeacherAllMark1.TabIndex = 0;
+            // 
+            // FTeacherMark
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1924, 1050);
+            this.Controls.Add(this.guna2CustomGradientPanel1);
+            this.Name = "FTeacherMark";
+            this.Text = "FTeacherMark";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.FTeacherMark_Load);
+            this.guna2CustomGradientPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2CustomGradientPanel guna2CustomGradientPanel1;
+        public TeacherUserControl.UcTeacherAllMark ucTeacherAllMark1;
+    }
+}
---
+++ WFThesisManagementSystem/Forms/TeacherViews/Views/FTeacherMark.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WFThesisManagementSystem.Forms.TeacherViews.TeacherUserControl;
+using WFThesisManagementSystem.Helper;
+using WFThesisManagementSystem.Models;
+using WFThesisManagementSystem.Repositories;
+using WFThesisManagementSystem.Services;
+
+namespace WFThesisManagementSystem.Forms.TeacherViews.Views
+{
+    public partial class FTeacherMark : Form
+    {
+        ThesisManagementContext _context;
+        TaskRepository _taskRepository;
+        StudentGroupRepository _studentGroupRepository;
+        ValidationInformationHelper _validationInformationHelper;
+        TaskService _taskService;
+        Dictionary<int, double> mark = new Dictionary<int, double>();
+
+        public FTeacherMark(ThesisManagementContext context)
+        {
+            _context = context;
+            _taskRepository = new TaskRepository(_context);
+            _studentGroupRepository = new StudentGroupRepository(_context);
+            _validationInformationHelper = new ValidationInformationHelper();
+            _taskService = new TaskService(_context);
+            InitializeComponent();
+            ucTeacherAllMark1.btnClose.Click += Close;
+            ucTeacherAllMark1.btnSave.Click += Save;
+        }
+        private void Close(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+        private void TaskLoad()
+        {
+            ucTeacherAllMark1.flpAllStudentMark.Controls.Clear();
+            foreach (var task in _taskRepository.GetAll())
+            {
+                UcTeacherSingleMark ucTeacherSingleMark = new UcTeacherSingleMark();
+                ucTeacherSingleMark.NameTask = task.task_name;
+                ucTeacherSingleMark.DescriptionTask = task.task_description;
+                ucTeacherSingleMark.NameStudent = _studentGroupRepository.GetById(task.group_id.Value).group_name.ToString();
+                ucTeacherSingleMark.lblCheck.ForeColor = Color.Black;
+                ucTeacherSingleMark.lblCheck.Text = "UnCompletely";
+                ucTeacherAllMark1.flpAllStudentMark.Controls.Add(ucTeacherSingleMark);
+            }
+            ucTeacherAllMark1.flpAllStudentMark.Show();
+        }
+        private void LoadSave()
+        {
+            foreach (var studentGroup in _studentGroupRepository.GetAll())
+            {
+                mark[studentGroup.group_id] = 0;
+            }
+            bool check = false;
+            foreach (var item in ucTeacherAllMark1.flpAllStudentMark.Controls)
+            {
+                if (item is UserControl)
+                {
+                    UcTeacherSingleMark ucTeacherSingleMark = (UcTeacherSingleMark)item;
+                    ucTeacherSingleMark.lblCheck.ForeColor = Color.Red;
+                    ucTeacherSingleMark.lblCheck.Text = "Completely";
+                    ucTeacherSingleMark.EditButton += EventEditButton;
+                    var task = _taskRepository.GetTaskByTaskName(ucTeacherSingleMark.NameTask);
+
+                    if (!string.IsNullOrEmpty(ucTeacherSingleMark.txtMark.Text))
+                    {
+                        if (_validationInformationHelper.CheckMarkValid(ucTeacherSingleMark.txtMark.Text) == 1)
+                        {
+                            mark[task.group_id.Value] += int.Parse(ucTeacherSingleMark.txtMark.Text);
+                            check = true;
+                        }
+                        else if (_validationInformationHelper.CheckMarkValid(ucTeacherSingleMark.txtMark.Text) == 2)
+                        {
+                            mark[task.group_id.Value] += double.Parse(ucTeacherSingleMark.txtMark.Text);
+                            check = true;
+                            
+                        }
+
+                    }
+                    ucTeacherSingleMark.txtMark.Enabled = false;
+                }
+
+            }
+            if (check) { MessageBox.Show("Save Mark Completely"); }
+            else MessageBox.Show("Mark Error", "Notification", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            foreach (var key in mark.Keys)
+            {
+                var studentgroup = _studentGroupRepository.GetById(key);
+                studentgroup.group_points = mark[key] / _taskService.NumberTask(key);
+                _studentGroupRepository.Update(studentgroup);
+            }
+        }
+        private void EventEditButton(object sender, EventArgs e)
+        {
+            UcTeacherSingleMark ucTeacherSingleMark = sender as UcTeacherSingleMark;
+            ucTeacherSingleMark.txtMark.Enabled = true;
+        }
+        private void Save(object sender, EventArgs e)
+        {
+            LoadSave();
+        }
+        private void FTeacherMark_Load(object sender, EventArgs e)
+        {
+            TaskLoad();
+            foreach (var studentGroup in _studentGroupRepository.GetAll())
+            {
+                mark[studentGroup.group_id] = 0;
+            }
+        }
+    }
+}
