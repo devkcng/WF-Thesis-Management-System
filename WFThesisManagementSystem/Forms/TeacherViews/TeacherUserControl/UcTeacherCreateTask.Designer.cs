56510b218cbf2b90817965c97ef42f6bd5cefb12
giahuynguyen28 <giahuy902@gmail.com>
2024-05-01 18:43:53 +0700

    change modifier of the label

--- WFThesisManagementSystem/Forms/TeacherViews/TeacherUserControl/UcTeacherCreateTask.Designer.cs
+++ WFThesisManagementSystem/Forms/TeacherViews/TeacherUserControl/UcTeacherCreateTask.Designer.cs
@@ -42,8 +42,8 @@
             this.txtTaskDescription = new Guna.UI2.WinForms.Guna2TextBox();
             this.lblTaskDescription = new System.Windows.Forms.Label();
             this.guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
-            this.label2 = new System.Windows.Forms.Label();
             this.cboGroupList = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.lblGroup = new System.Windows.Forms.Label();
             this.guna2Panel3.SuspendLayout();
             this.guna2Panel1.SuspendLayout();
             this.guna2Panel5.SuspendLayout();
@@ -55,21 +55,21 @@
             // 
             this.guna2Panel3.Controls.Add(this.lblEndDate);
             this.guna2Panel3.Controls.Add(this.dtpEndDate);
-            this.guna2Panel3.Location = new System.Drawing.Point(100, 224);
-            this.guna2Panel3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.guna2Panel3.Location = new System.Drawing.Point(267, 534);
+            this.guna2Panel3.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.guna2Panel3.Name = "guna2Panel3";
-            this.guna2Panel3.Size = new System.Drawing.Size(424, 60);
+            this.guna2Panel3.Size = new System.Drawing.Size(1131, 143);
             this.guna2Panel3.TabIndex = 14;
             // 
             // lblEndDate
             // 
             this.lblEndDate.AutoSize = true;
-            this.lblEndDate.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEndDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblEndDate.ForeColor = System.Drawing.Color.White;
-            this.lblEndDate.Location = new System.Drawing.Point(13, 0);
-            this.lblEndDate.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblEndDate.Location = new System.Drawing.Point(35, 0);
+            this.lblEndDate.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.lblEndDate.Name = "lblEndDate";
-            this.lblEndDate.Size = new System.Drawing.Size(89, 19);
+            this.lblEndDate.Size = new System.Drawing.Size(222, 46);
             this.lblEndDate.TabIndex = 56;
             this.lblEndDate.Text = "End Date *";
             // 
@@ -83,12 +83,12 @@
             this.dtpEndDate.FillColor = System.Drawing.Color.White;
             this.dtpEndDate.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.dtpEndDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dtpEndDate.Location = new System.Drawing.Point(16, 27);
-            this.dtpEndDate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dtpEndDate.Location = new System.Drawing.Point(43, 64);
+            this.dtpEndDate.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.dtpEndDate.MaxDate = new System.DateTime(9998, 12, 31, 0, 0, 0, 0);
             this.dtpEndDate.MinDate = new System.DateTime(1753, 1, 1, 0, 0, 0, 0);
             this.dtpEndDate.Name = "dtpEndDate";
-            this.dtpEndDate.Size = new System.Drawing.Size(390, 34);
+            this.dtpEndDate.Size = new System.Drawing.Size(1040, 81);
             this.dtpEndDate.TabIndex = 54;
             this.dtpEndDate.Value = new System.DateTime(2024, 3, 13, 10, 7, 4, 277);
             // 
@@ -96,10 +96,10 @@
             // 
             this.guna2Panel1.Controls.Add(this.txtTaskName);
             this.guna2Panel1.Controls.Add(this.lblTaskName);
-            this.guna2Panel1.Location = new System.Drawing.Point(100, 133);
-            this.guna2Panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.guna2Panel1.Location = new System.Drawing.Point(267, 317);
+            this.guna2Panel1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.guna2Panel1.Name = "guna2Panel1";
-            this.guna2Panel1.Size = new System.Drawing.Size(423, 60);
+            this.guna2Panel1.Size = new System.Drawing.Size(1128, 143);
             this.guna2Panel1.TabIndex = 13;
             // 
             // txtTaskName
@@ -117,24 +117,24 @@
             this.txtTaskName.Font = new System.Drawing.Font("Segoe UI", 12F);
             this.txtTaskName.ForeColor = System.Drawing.Color.Black;
             this.txtTaskName.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.txtTaskName.Location = new System.Drawing.Point(14, 31);
-            this.txtTaskName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtTaskName.Location = new System.Drawing.Point(37, 74);
+            this.txtTaskName.Margin = new System.Windows.Forms.Padding(8, 10, 8, 10);
             this.txtTaskName.Name = "txtTaskName";
             this.txtTaskName.PasswordChar = '\0';
             this.txtTaskName.PlaceholderText = "";
             this.txtTaskName.SelectedText = "";
-            this.txtTaskName.Size = new System.Drawing.Size(393, 25);
+            this.txtTaskName.Size = new System.Drawing.Size(1048, 60);
             this.txtTaskName.TabIndex = 1;
             // 
             // lblTaskName
             // 
             this.lblTaskName.AutoSize = true;
-            this.lblTaskName.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTaskName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblTaskName.ForeColor = System.Drawing.Color.White;
-            this.lblTaskName.Location = new System.Drawing.Point(13, 8);
-            this.lblTaskName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTaskName.Location = new System.Drawing.Point(35, 19);
+            this.lblTaskName.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.lblTaskName.Name = "lblTaskName";
-            this.lblTaskName.Size = new System.Drawing.Size(106, 19);
+            this.lblTaskName.Size = new System.Drawing.Size(260, 46);
             this.lblTaskName.TabIndex = 0;
             this.lblTaskName.Text = "Task Name *";
             // 
@@ -142,10 +142,10 @@
             // 
             this.guna2Panel5.Controls.Add(this.btnSave);
             this.guna2Panel5.Controls.Add(this.btnClose);
-            this.guna2Panel5.Location = new System.Drawing.Point(411, 419);
-            this.guna2Panel5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.guna2Panel5.Location = new System.Drawing.Point(1096, 999);
+            this.guna2Panel5.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.guna2Panel5.Name = "guna2Panel5";
-            this.guna2Panel5.Size = new System.Drawing.Size(169, 36);
+            this.guna2Panel5.Size = new System.Drawing.Size(451, 86);
             this.guna2Panel5.TabIndex = 16;
             // 
             // btnSave
@@ -158,9 +158,9 @@
             this.btnSave.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSave.ForeColor = System.Drawing.Color.White;
             this.btnSave.Location = new System.Drawing.Point(0, 0);
-            this.btnSave.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(69, 36);
+            this.btnSave.Size = new System.Drawing.Size(184, 86);
             this.btnSave.TabIndex = 8;
             this.btnSave.Text = "Save";
             // 
@@ -174,22 +174,22 @@
             this.btnClose.FillColor = System.Drawing.Color.WhiteSmoke;
             this.btnClose.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnClose.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.btnClose.Location = new System.Drawing.Point(88, 0);
-            this.btnClose.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnClose.Location = new System.Drawing.Point(235, 0);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(81, 36);
+            this.btnClose.Size = new System.Drawing.Size(216, 86);
             this.btnClose.TabIndex = 7;
             this.btnClose.Text = "Close";
             // 
             // lblTitle
             // 
             this.lblTitle.AutoSize = true;
-            this.lblTitle.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblTitle.ForeColor = System.Drawing.Color.White;
-            this.lblTitle.Location = new System.Drawing.Point(234, 18);
-            this.lblTitle.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTitle.Location = new System.Drawing.Point(624, 43);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(122, 19);
+            this.lblTitle.Size = new System.Drawing.Size(349, 46);
             this.lblTitle.TabIndex = 52;
             this.lblTitle.Text = "CREATE A TASK";
             // 
@@ -197,10 +197,10 @@
             // 
             this.guna2Panel4.Controls.Add(this.txtTaskDescription);
             this.guna2Panel4.Controls.Add(this.lblTaskDescription);
-            this.guna2Panel4.Location = new System.Drawing.Point(101, 311);
-            this.guna2Panel4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.guna2Panel4.Location = new System.Drawing.Point(269, 742);
+            this.guna2Panel4.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.guna2Panel4.Name = "guna2Panel4";
-            this.guna2Panel4.Size = new System.Drawing.Size(423, 97);
+            this.guna2Panel4.Size = new System.Drawing.Size(1128, 231);
             this.guna2Panel4.TabIndex = 56;
             // 
             // txtTaskDescription
@@ -218,47 +218,36 @@
             this.txtTaskDescription.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtTaskDescription.ForeColor = System.Drawing.Color.Black;
             this.txtTaskDescription.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.txtTaskDescription.Location = new System.Drawing.Point(13, 27);
+            this.txtTaskDescription.Location = new System.Drawing.Point(35, 64);
+            this.txtTaskDescription.Margin = new System.Windows.Forms.Padding(21, 24, 21, 24);
             this.txtTaskDescription.Name = "txtTaskDescription";
             this.txtTaskDescription.PasswordChar = '\0';
             this.txtTaskDescription.PlaceholderText = "";
             this.txtTaskDescription.SelectedText = "";
-            this.txtTaskDescription.Size = new System.Drawing.Size(393, 60);
+            this.txtTaskDescription.Size = new System.Drawing.Size(1048, 143);
             this.txtTaskDescription.TabIndex = 7;
             // 
             // lblTaskDescription
             // 
             this.lblTaskDescription.AutoSize = true;
-            this.lblTaskDescription.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTaskDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblTaskDescription.ForeColor = System.Drawing.Color.White;
-            this.lblTaskDescription.Location = new System.Drawing.Point(12, 5);
-            this.lblTaskDescription.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTaskDescription.Location = new System.Drawing.Point(32, 12);
+            this.lblTaskDescription.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.lblTaskDescription.Name = "lblTaskDescription";
-            this.lblTaskDescription.Size = new System.Drawing.Size(142, 19);
+            this.lblTaskDescription.Size = new System.Drawing.Size(362, 46);
             this.lblTaskDescription.TabIndex = 6;
             this.lblTaskDescription.Text = "Task Description *";
             // 
             // guna2Panel2
             // 
             this.guna2Panel2.Controls.Add(this.cboGroupList);
-            this.guna2Panel2.Controls.Add(this.label2);
-            this.guna2Panel2.Location = new System.Drawing.Point(101, 50);
-            this.guna2Panel2.Margin = new System.Windows.Forms.Padding(2);
+            this.guna2Panel2.Controls.Add(this.lblGroup);
+            this.guna2Panel2.Location = new System.Drawing.Point(269, 119);
+            this.guna2Panel2.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.guna2Panel2.Name = "guna2Panel2";
-            this.guna2Panel2.Size = new System.Drawing.Size(423, 60);
+            this.guna2Panel2.Size = new System.Drawing.Size(1128, 143);
             this.guna2Panel2.TabIndex = 14;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(9, 2);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(69, 19);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Group *";
             // 
             // cboGroupList
             // 
@@ -272,14 +261,27 @@
             this.cboGroupList.Font = new System.Drawing.Font("Segoe UI", 10F);
             this.cboGroupList.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
             this.cboGroupList.ItemHeight = 30;
-            this.cboGroupList.Location = new System.Drawing.Point(16, 24);
+            this.cboGroupList.Location = new System.Drawing.Point(43, 57);
+            this.cboGroupList.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.cboGroupList.Name = "cboGroupList";
-            this.cboGroupList.Size = new System.Drawing.Size(389, 36);
+            this.cboGroupList.Size = new System.Drawing.Size(1031, 36);
             this.cboGroupList.TabIndex = 1;
+            // 
+            // lblGroup
+            // 
+            this.lblGroup.AutoSize = true;
+            this.lblGroup.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGroup.ForeColor = System.Drawing.Color.White;
+            this.lblGroup.Location = new System.Drawing.Point(24, 5);
+            this.lblGroup.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblGroup.Name = "lblGroup";
+            this.lblGroup.Size = new System.Drawing.Size(164, 46);
+            this.lblGroup.TabIndex = 0;
+            this.lblGroup.Text = "Group *";
             // 
             // UcTeacherCreateTask
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightSeaGreen;
             this.Controls.Add(this.guna2Panel2);
@@ -288,9 +290,9 @@
             this.Controls.Add(this.guna2Panel5);
             this.Controls.Add(this.guna2Panel3);
             this.Controls.Add(this.guna2Panel1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.Name = "UcTeacherCreateTask";
-            this.Size = new System.Drawing.Size(605, 477);
+            this.Size = new System.Drawing.Size(1613, 1137);
             this.guna2Panel3.ResumeLayout(false);
             this.guna2Panel3.PerformLayout();
             this.guna2Panel1.ResumeLayout(false);
@@ -321,6 +323,6 @@
         private System.Windows.Forms.Label lblEndDate;
         private Guna.UI2.WinForms.Guna2Panel guna2Panel2;
         public Guna.UI2.WinForms.Guna2ComboBox cboGroupList;
-        private System.Windows.Forms.Label label2;
+        public System.Windows.Forms.Label lblGroup;
     }
 }
