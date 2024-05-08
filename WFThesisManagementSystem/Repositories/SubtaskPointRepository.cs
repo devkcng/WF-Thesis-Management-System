bbfc81b44f54feef27b662dbea5f961e379d7906
devkcng <kimmanhcuong230304@gmail.com>
2024-05-08 17:13:27 +0700

    feat: create SubtaskPointRepository.cs

---
+++ WFThesisManagementSystem/Repositories/SubtaskPointRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WFThesisManagementSystem.Models;
+
+namespace WFThesisManagementSystem.Repositories
+{
+    public class SubtaskPointRepository : IRepository<SubtaskPoint>
+    {
+        ThesisManagementContext _context;
+
+        public SubtaskPointRepository(ThesisManagementContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(SubtaskPoint entity)
+        {
+            _context.SubtaskPoints.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Delete(SubtaskPoint entity)
+        {
+            _context.SubtaskPoints.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public IQueryable<SubtaskPoint> GetAll()
+        {
+            return _context.SubtaskPoints;
+        }
+
+        public SubtaskPoint GetById(int id)
+        {
+            return _context.SubtaskPoints.FirstOrDefault(x => x.subtaskpoint_id == id);
+        }
+
+        public void Update(SubtaskPoint entity)
+        {
+             _context.Entry(entity).State = System.Data.Entity.EntityState.Modified;
+            _context.SaveChanges();
+        }
+    }
+}
