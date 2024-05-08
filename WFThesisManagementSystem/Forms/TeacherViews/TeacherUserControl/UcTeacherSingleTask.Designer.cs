6027d46cbc3b21833fd863e77ec4fc8ce44fdd0c
KienLoveNH <kientri098@gmail.com>
2024-05-08 12:38:31 +0700

    Add properties task id

--- WFThesisManagementSystem/Forms/TeacherViews/TeacherUserControl/UcTeacherSingleTask.Designer.cs
+++ WFThesisManagementSystem/Forms/TeacherViews/TeacherUserControl/UcTeacherSingleTask.Designer.cs
@@ -31,19 +31,21 @@
             this.dtpFrom = new System.Windows.Forms.Label();
             this.dtpTo = new System.Windows.Forms.Label();
             this.guna2CustomGradientPanel1 = new Guna.UI2.WinForms.Guna2CustomGradientPanel();
-            this.guna2CustomGradientPanel2 = new Guna.UI2.WinForms.Guna2CustomGradientPanel();
-            this.lblName = new System.Windows.Forms.Label();
-            this.ptbImage = new System.Windows.Forms.PictureBox();
-            this.btnEdit = new Guna.UI2.WinForms.Guna2PictureBox();
-            this.btnChat = new Guna.UI2.WinForms.Guna2PictureBox();
-            this.lblContentTask = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
+            this.btnPoint = new Guna.UI2.WinForms.Guna2PictureBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblContentTask = new System.Windows.Forms.Label();
+            this.guna2CustomGradientPanel2 = new Guna.UI2.WinForms.Guna2CustomGradientPanel();
+            this.btnChat = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.btnEdit = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.ptbImage = new System.Windows.Forms.PictureBox();
+            this.lblName = new System.Windows.Forms.Label();
             this.guna2CustomGradientPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnPoint)).BeginInit();
             this.guna2CustomGradientPanel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.ptbImage)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.btnEdit)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnChat)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnEdit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbImage)).BeginInit();
             this.SuspendLayout();
             // 
             // dtpFrom
@@ -52,11 +54,10 @@
             this.dtpFrom.AutoSize = true;
             this.dtpFrom.BackColor = System.Drawing.Color.White;
             this.dtpFrom.Font = new System.Drawing.Font("Cambria", 9.900001F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dtpFrom.Location = new System.Drawing.Point(203, 299);
-            this.dtpFrom.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.dtpFrom.Location = new System.Drawing.Point(114, 193);
             this.dtpFrom.Name = "dtpFrom";
             this.dtpFrom.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.dtpFrom.Size = new System.Drawing.Size(113, 40);
+            this.dtpFrom.Size = new System.Drawing.Size(68, 23);
             this.dtpFrom.TabIndex = 31;
             this.dtpFrom.Text = "label1";
             // 
@@ -67,11 +68,10 @@
             this.dtpTo.BackColor = System.Drawing.Color.White;
             this.dtpTo.Font = new System.Drawing.Font("Cambria", 9.900001F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dtpTo.ForeColor = System.Drawing.Color.Red;
-            this.dtpTo.Location = new System.Drawing.Point(662, 299);
-            this.dtpTo.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.dtpTo.Location = new System.Drawing.Point(372, 193);
             this.dtpTo.Name = "dtpTo";
             this.dtpTo.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.dtpTo.Size = new System.Drawing.Size(113, 40);
+            this.dtpTo.Size = new System.Drawing.Size(68, 23);
             this.dtpTo.TabIndex = 32;
             this.dtpTo.Text = "label2";
             // 
@@ -79,6 +79,7 @@
             // 
             this.guna2CustomGradientPanel1.BorderColor = System.Drawing.Color.Black;
             this.guna2CustomGradientPanel1.BorderThickness = 2;
+            this.guna2CustomGradientPanel1.Controls.Add(this.btnPoint);
             this.guna2CustomGradientPanel1.Controls.Add(this.label2);
             this.guna2CustomGradientPanel1.Controls.Add(this.label1);
             this.guna2CustomGradientPanel1.Controls.Add(this.lblContentTask);
@@ -86,11 +87,64 @@
             this.guna2CustomGradientPanel1.Controls.Add(this.dtpTo);
             this.guna2CustomGradientPanel1.Controls.Add(this.dtpFrom);
             this.guna2CustomGradientPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.guna2CustomGradientPanel1.Location = new System.Drawing.Point(0, 20);
+            this.guna2CustomGradientPanel1.Location = new System.Drawing.Point(0, 13);
+            this.guna2CustomGradientPanel1.Margin = new System.Windows.Forms.Padding(2);
             this.guna2CustomGradientPanel1.Name = "guna2CustomGradientPanel1";
-            this.guna2CustomGradientPanel1.Padding = new System.Windows.Forms.Padding(10);
-            this.guna2CustomGradientPanel1.Size = new System.Drawing.Size(1404, 349);
+            this.guna2CustomGradientPanel1.Padding = new System.Windows.Forms.Padding(6);
+            this.guna2CustomGradientPanel1.Size = new System.Drawing.Size(790, 225);
             this.guna2CustomGradientPanel1.TabIndex = 0;
+            // 
+            // btnPoint
+            // 
+            this.btnPoint.Image = global::WFThesisManagementSystem.Properties.Resources.icons8_mark_641;
+            this.btnPoint.ImageRotate = 0F;
+            this.btnPoint.Location = new System.Drawing.Point(655, 113);
+            this.btnPoint.Name = "btnPoint";
+            this.btnPoint.Size = new System.Drawing.Size(64, 64);
+            this.btnPoint.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.btnPoint.TabIndex = 39;
+            this.btnPoint.TabStop = false;
+            this.btnPoint.Click += new System.EventHandler(this.Point_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Cambria", 9.900001F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(285, 193);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(86, 23);
+            this.label2.TabIndex = 38;
+            this.label2.Text = "Due Day";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Cambria", 9.900001F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(15, 193);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(98, 23);
+            this.label1.TabIndex = 37;
+            this.label1.Text = "Open Day";
+            // 
+            // lblContentTask
+            // 
+            this.lblContentTask.AutoEllipsis = true;
+            this.lblContentTask.AutoSize = true;
+            this.lblContentTask.BackColor = System.Drawing.Color.White;
+            this.lblContentTask.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblContentTask.Location = new System.Drawing.Point(15, 84);
+            this.lblContentTask.MaximumSize = new System.Drawing.Size(675, 65);
+            this.lblContentTask.Name = "lblContentTask";
+            this.lblContentTask.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lblContentTask.Size = new System.Drawing.Size(155, 28);
+            this.lblContentTask.TabIndex = 36;
+            this.lblContentTask.Text = "Content Task";
             // 
             // guna2CustomGradientPanel2
             // 
@@ -99,48 +153,11 @@
             this.guna2CustomGradientPanel2.Controls.Add(this.ptbImage);
             this.guna2CustomGradientPanel2.Controls.Add(this.lblName);
             this.guna2CustomGradientPanel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.guna2CustomGradientPanel2.Location = new System.Drawing.Point(10, 10);
+            this.guna2CustomGradientPanel2.Location = new System.Drawing.Point(6, 6);
+            this.guna2CustomGradientPanel2.Margin = new System.Windows.Forms.Padding(2);
             this.guna2CustomGradientPanel2.Name = "guna2CustomGradientPanel2";
-            this.guna2CustomGradientPanel2.Size = new System.Drawing.Size(1384, 87);
+            this.guna2CustomGradientPanel2.Size = new System.Drawing.Size(778, 56);
             this.guna2CustomGradientPanel2.TabIndex = 34;
-            // 
-            // lblName
-            // 
-            this.lblName.AutoSize = true;
-            this.lblName.BackColor = System.Drawing.Color.White;
-            this.lblName.Font = new System.Drawing.Font("Cambria", 14.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblName.Location = new System.Drawing.Point(89, 19);
-            this.lblName.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
-            this.lblName.Name = "lblName";
-            this.lblName.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.lblName.Size = new System.Drawing.Size(791, 55);
-            this.lblName.TabIndex = 27;
-            this.lblName.Text = "Make an automatic payment system";
-            // 
-            // ptbImage
-            // 
-            this.ptbImage.BackColor = System.Drawing.Color.White;
-            this.ptbImage.Image = global::WFThesisManagementSystem.Properties.Resources.Idea;
-            this.ptbImage.Location = new System.Drawing.Point(17, 19);
-            this.ptbImage.Margin = new System.Windows.Forms.Padding(5);
-            this.ptbImage.Name = "ptbImage";
-            this.ptbImage.Size = new System.Drawing.Size(62, 55);
-            this.ptbImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.ptbImage.TabIndex = 28;
-            this.ptbImage.TabStop = false;
-            // 
-            // btnEdit
-            // 
-            this.btnEdit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnEdit.BackColor = System.Drawing.Color.White;
-            this.btnEdit.Image = global::WFThesisManagementSystem.Properties.Resources.pencil1;
-            this.btnEdit.ImageRotate = 0F;
-            this.btnEdit.Location = new System.Drawing.Point(1184, 19);
-            this.btnEdit.Name = "btnEdit";
-            this.btnEdit.Size = new System.Drawing.Size(62, 55);
-            this.btnEdit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.btnEdit.TabIndex = 34;
-            this.btnEdit.TabStop = false;
             // 
             // btnChat
             // 
@@ -148,72 +165,67 @@
             this.btnChat.BackColor = System.Drawing.Color.White;
             this.btnChat.Image = global::WFThesisManagementSystem.Properties.Resources.Chat1;
             this.btnChat.ImageRotate = 0F;
-            this.btnChat.Location = new System.Drawing.Point(1286, 19);
+            this.btnChat.Location = new System.Drawing.Point(723, 12);
+            this.btnChat.Margin = new System.Windows.Forms.Padding(2);
             this.btnChat.Name = "btnChat";
-            this.btnChat.Size = new System.Drawing.Size(62, 55);
+            this.btnChat.Size = new System.Drawing.Size(35, 35);
             this.btnChat.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.btnChat.TabIndex = 35;
             this.btnChat.TabStop = false;
             // 
-            // lblContentTask
+            // btnEdit
             // 
-            this.lblContentTask.AutoEllipsis = true;
-            this.lblContentTask.AutoSize = true;
-            this.lblContentTask.BackColor = System.Drawing.Color.White;
-            this.lblContentTask.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblContentTask.Location = new System.Drawing.Point(27, 130);
-            this.lblContentTask.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
-            this.lblContentTask.MaximumSize = new System.Drawing.Size(1200, 100);
-            this.lblContentTask.Name = "lblContentTask";
-            this.lblContentTask.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.lblContentTask.Size = new System.Drawing.Size(259, 47);
-            this.lblContentTask.TabIndex = 36;
-            this.lblContentTask.Text = "Content Task";
+            this.btnEdit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnEdit.BackColor = System.Drawing.Color.White;
+            this.btnEdit.Image = global::WFThesisManagementSystem.Properties.Resources.pencil1;
+            this.btnEdit.ImageRotate = 0F;
+            this.btnEdit.Location = new System.Drawing.Point(666, 12);
+            this.btnEdit.Margin = new System.Windows.Forms.Padding(2);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(35, 35);
+            this.btnEdit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnEdit.TabIndex = 34;
+            this.btnEdit.TabStop = false;
             // 
-            // label1
+            // ptbImage
             // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.White;
-            this.label1.Font = new System.Drawing.Font("Cambria", 9.900001F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(27, 299);
-            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
-            this.label1.Name = "label1";
-            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.label1.Size = new System.Drawing.Size(166, 40);
-            this.label1.TabIndex = 37;
-            this.label1.Text = "Open Day";
+            this.ptbImage.BackColor = System.Drawing.Color.White;
+            this.ptbImage.Image = global::WFThesisManagementSystem.Properties.Resources.Idea;
+            this.ptbImage.Location = new System.Drawing.Point(10, 12);
+            this.ptbImage.Name = "ptbImage";
+            this.ptbImage.Size = new System.Drawing.Size(35, 35);
+            this.ptbImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbImage.TabIndex = 28;
+            this.ptbImage.TabStop = false;
             // 
-            // label2
+            // lblName
             // 
-            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.White;
-            this.label2.Font = new System.Drawing.Font("Cambria", 9.900001F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(507, 299);
-            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
-            this.label2.Name = "label2";
-            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.label2.Size = new System.Drawing.Size(145, 40);
-            this.label2.TabIndex = 38;
-            this.label2.Text = "Due Day";
+            this.lblName.AutoSize = true;
+            this.lblName.BackColor = System.Drawing.Color.White;
+            this.lblName.Font = new System.Drawing.Font("Cambria", 14.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(50, 12);
+            this.lblName.Name = "lblName";
+            this.lblName.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lblName.Size = new System.Drawing.Size(493, 34);
+            this.lblName.TabIndex = 27;
+            this.lblName.Text = "Make an automatic payment system";
             // 
             // UcTeacherSingleTask
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.guna2CustomGradientPanel1);
-            this.Margin = new System.Windows.Forms.Padding(5);
             this.Name = "UcTeacherSingleTask";
-            this.Padding = new System.Windows.Forms.Padding(0, 20, 0, 20);
-            this.Size = new System.Drawing.Size(1404, 389);
+            this.Padding = new System.Windows.Forms.Padding(0, 13, 0, 13);
+            this.Size = new System.Drawing.Size(790, 251);
             this.guna2CustomGradientPanel1.ResumeLayout(false);
             this.guna2CustomGradientPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnPoint)).EndInit();
             this.guna2CustomGradientPanel2.ResumeLayout(false);
             this.guna2CustomGradientPanel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.ptbImage)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.btnEdit)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnChat)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnEdit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbImage)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -230,5 +242,6 @@
         public System.Windows.Forms.Label lblContentTask;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2PictureBox btnPoint;
     }
 }
