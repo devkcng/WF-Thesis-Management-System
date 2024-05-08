73009e496e7f1b60e6e2071829dd1d9ba2e14380
devkcng <kimmanhcuong230304@gmail.com>
2024-05-09 00:44:45 +0700

    refactor: change location and size of btnChat

--- WFThesisManagementSystem/Forms/StudentViews/StudentUserControl/UCTask.Designer.cs
+++ WFThesisManagementSystem/Forms/StudentViews/StudentUserControl/UCTask.Designer.cs
@@ -48,8 +48,8 @@
             this.panelTask.BorderColor = System.Drawing.Color.Black;
             this.panelTask.BorderRadius = 10;
             this.panelTask.BorderThickness = 2;
-            this.panelTask.Controls.Add(this.txtProjectDetail);
             this.panelTask.Controls.Add(this.btnChat);
+            this.panelTask.Controls.Add(this.txtProjectDetail);
             this.panelTask.Controls.Add(this.lblOpenDay);
             this.panelTask.Controls.Add(this.lblDeadline);
             this.panelTask.Controls.Add(this.ptbStatus);
@@ -60,9 +60,10 @@
             this.panelTask.CustomBorderColor = System.Drawing.Color.Black;
             this.panelTask.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelTask.Font = new System.Drawing.Font("Cambria", 9.900001F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.panelTask.Location = new System.Drawing.Point(10, 20);
+            this.panelTask.Location = new System.Drawing.Point(4, 8);
+            this.panelTask.Margin = new System.Windows.Forms.Padding(1);
             this.panelTask.Name = "panelTask";
-            this.panelTask.Size = new System.Drawing.Size(1319, 310);
+            this.panelTask.Size = new System.Drawing.Size(494, 131);
             this.panelTask.TabIndex = 0;
             // 
             // txtProjectDetail
@@ -77,9 +78,8 @@
             this.txtProjectDetail.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.txtProjectDetail.Font = new System.Drawing.Font("Cambria", 10F);
             this.txtProjectDetail.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.txtProjectDetail.Location = new System.Drawing.Point(44, 84);
-            this.txtProjectDetail.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
-            this.txtProjectDetail.MaximumSize = new System.Drawing.Size(1200, 150);
+            this.txtProjectDetail.Location = new System.Drawing.Point(16, 35);
+            this.txtProjectDetail.MaximumSize = new System.Drawing.Size(450, 63);
             this.txtProjectDetail.Multiline = true;
             this.txtProjectDetail.Name = "txtProjectDetail";
             this.txtProjectDetail.PasswordChar = '\0';
@@ -87,7 +87,7 @@
             this.txtProjectDetail.ReadOnly = true;
             this.txtProjectDetail.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.txtProjectDetail.SelectedText = "";
-            this.txtProjectDetail.Size = new System.Drawing.Size(1200, 147);
+            this.txtProjectDetail.Size = new System.Drawing.Size(450, 62);
             this.txtProjectDetail.TabIndex = 81;
             // 
             // btnChat
@@ -96,10 +96,11 @@
             this.btnChat.BackColor = System.Drawing.Color.White;
             this.btnChat.Image = global::WFThesisManagementSystem.Properties.Resources.Chat1;
             this.btnChat.ImageRotate = 0F;
-            this.btnChat.Location = new System.Drawing.Point(1236, 19);
+            this.btnChat.Location = new System.Drawing.Point(431, 0);
+            this.btnChat.Margin = new System.Windows.Forms.Padding(1);
             this.btnChat.Name = "btnChat";
             this.btnChat.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
-            this.btnChat.Size = new System.Drawing.Size(50, 55);
+            this.btnChat.Size = new System.Drawing.Size(52, 46);
             this.btnChat.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.btnChat.TabIndex = 80;
             this.btnChat.TabStop = false;
@@ -110,9 +111,10 @@
             this.lblOpenDay.AutoSize = true;
             this.lblOpenDay.BackColor = System.Drawing.Color.White;
             this.lblOpenDay.Font = new System.Drawing.Font("Cambria", 9.900001F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblOpenDay.Location = new System.Drawing.Point(217, 252);
+            this.lblOpenDay.Location = new System.Drawing.Point(81, 107);
+            this.lblOpenDay.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.lblOpenDay.Name = "lblOpenDay";
-            this.lblOpenDay.Size = new System.Drawing.Size(171, 40);
+            this.lblOpenDay.Size = new System.Drawing.Size(69, 16);
             this.lblOpenDay.TabIndex = 77;
             this.lblOpenDay.Text = "3/4/2024";
             // 
@@ -123,9 +125,10 @@
             this.lblDeadline.BackColor = System.Drawing.Color.White;
             this.lblDeadline.Font = new System.Drawing.Font("Cambria", 9.900001F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblDeadline.ForeColor = System.Drawing.Color.Red;
-            this.lblDeadline.Location = new System.Drawing.Point(614, 252);
+            this.lblDeadline.Location = new System.Drawing.Point(230, 107);
+            this.lblDeadline.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.lblDeadline.Name = "lblDeadline";
-            this.lblDeadline.Size = new System.Drawing.Size(171, 40);
+            this.lblDeadline.Size = new System.Drawing.Size(69, 16);
             this.lblDeadline.TabIndex = 76;
             this.lblDeadline.Text = "5/4/2024";
             // 
@@ -135,10 +138,11 @@
             this.ptbStatus.BackColor = System.Drawing.Color.White;
             this.ptbStatus.Image = global::WFThesisManagementSystem.Properties.Resources.photo_2024_04_02_16_52_38;
             this.ptbStatus.ImageRotate = 0F;
-            this.ptbStatus.Location = new System.Drawing.Point(1226, 252);
+            this.ptbStatus.Location = new System.Drawing.Point(459, 107);
+            this.ptbStatus.Margin = new System.Windows.Forms.Padding(1);
             this.ptbStatus.Name = "ptbStatus";
             this.ptbStatus.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
-            this.ptbStatus.Size = new System.Drawing.Size(36, 33);
+            this.ptbStatus.Size = new System.Drawing.Size(14, 14);
             this.ptbStatus.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.ptbStatus.TabIndex = 75;
             this.ptbStatus.TabStop = false;
@@ -149,10 +153,10 @@
             this.lblStatus.AutoSize = true;
             this.lblStatus.BackColor = System.Drawing.Color.White;
             this.lblStatus.Font = new System.Drawing.Font("Cambria", 9.900001F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblStatus.Location = new System.Drawing.Point(1112, 252);
-            this.lblStatus.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblStatus.Location = new System.Drawing.Point(416, 107);
+            this.lblStatus.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblStatus.Name = "lblStatus";
-            this.lblStatus.Size = new System.Drawing.Size(112, 40);
+            this.lblStatus.Size = new System.Drawing.Size(45, 16);
             this.lblStatus.TabIndex = 74;
             this.lblStatus.Text = "Status";
             // 
@@ -162,10 +166,10 @@
             this.lblDueDate.AutoSize = true;
             this.lblDueDate.BackColor = System.Drawing.Color.White;
             this.lblDueDate.Font = new System.Drawing.Font("Cambria", 9.900001F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDueDate.Location = new System.Drawing.Point(443, 252);
-            this.lblDueDate.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblDueDate.Location = new System.Drawing.Point(166, 107);
+            this.lblDueDate.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblDueDate.Name = "lblDueDate";
-            this.lblDueDate.Size = new System.Drawing.Size(158, 40);
+            this.lblDueDate.Size = new System.Drawing.Size(64, 16);
             this.lblDueDate.TabIndex = 73;
             this.lblDueDate.Text = "Due Date";
             // 
@@ -175,10 +179,10 @@
             this.lblOpenDate.AutoSize = true;
             this.lblOpenDate.BackColor = System.Drawing.Color.White;
             this.lblOpenDate.Font = new System.Drawing.Font("Cambria", 9.900001F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblOpenDate.Location = new System.Drawing.Point(37, 252);
-            this.lblOpenDate.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblOpenDate.Location = new System.Drawing.Point(14, 107);
+            this.lblOpenDate.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblOpenDate.Name = "lblOpenDate";
-            this.lblOpenDate.Size = new System.Drawing.Size(166, 40);
+            this.lblOpenDate.Size = new System.Drawing.Size(67, 16);
             this.lblOpenDate.TabIndex = 72;
             this.lblOpenDate.Text = "Open Day";
             // 
@@ -187,22 +191,22 @@
             this.lblName.AutoSize = true;
             this.lblName.BackColor = System.Drawing.Color.White;
             this.lblName.Font = new System.Drawing.Font("Cambria", 14.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblName.Location = new System.Drawing.Point(33, 19);
-            this.lblName.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblName.Location = new System.Drawing.Point(12, 8);
+            this.lblName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(791, 55);
+            this.lblName.Size = new System.Drawing.Size(319, 22);
             this.lblName.TabIndex = 70;
             this.lblName.Text = "Make an automatic payment system";
             // 
             // UCTask
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.panelTask);
-            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "UCTask";
-            this.Padding = new System.Windows.Forms.Padding(10, 20, 10, 20);
-            this.Size = new System.Drawing.Size(1339, 350);
+            this.Padding = new System.Windows.Forms.Padding(4, 8, 4, 8);
+            this.Size = new System.Drawing.Size(502, 147);
             this.Click += new System.EventHandler(this.UCTask_CLick);
             this.panelTask.ResumeLayout(false);
             this.panelTask.PerformLayout();
