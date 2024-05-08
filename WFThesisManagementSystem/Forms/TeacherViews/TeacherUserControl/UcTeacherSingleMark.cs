b08fb2a7ff0382d636c48c0a3b87a6bd283c3bab
KienLoveNH <kientri098@gmail.com>
2024-05-08 12:39:24 +0700

    ignore command line

--- WFThesisManagementSystem/Forms/TeacherViews/TeacherUserControl/UcTeacherSingleMark.cs
+++ WFThesisManagementSystem/Forms/TeacherViews/TeacherUserControl/UcTeacherSingleMark.cs
@@ -9,30 +9,30 @@ namespace WFThesisManagementSystem.Forms.TeacherViews.TeacherUserControl
         public UcTeacherSingleMark()
         {
             InitializeComponent();
-            btnEdit.Click += EditButton_Click;
+            //btnEdit.Click += EditButton_Click;
         }
         #region Properties
         private string nameTask;
         private string descriptionTask;
         private string nameStudent;
         private double mark;
-        public string NameTask
-        {
-            set { nameTask = value; lblTaskName.Text = value; }
-            get { return nameTask; }
-        }
-        public string DescriptionTask
-        {
-            set { descriptionTask = value; lblTaskDescription.Text = value; }
-        }
-        public string NameStudent
-        {
-            set { nameStudent = value; lblTaskName.Text = value; }
-        }
-        public double Mark
-        {
-            set { mark = value; }
-        }
+        //public string NameTask
+        //{
+        //    set { nameTask = value; lblPoint.Text = value; }
+        //    get { return nameTask; }
+        //}
+        //public string DescriptionTask
+        //{
+        //    set { descriptionTask = value; lblReview.Text = value; }
+        //}
+        //public string NameStudent
+        //{
+        //    set { nameStudent = value; lblPoint.Text = value; }
+        //}
+        //public double Mark
+        //{
+        //    set { mark = value; }
+        //}
         #endregion
         #region EventEditButton
         private void EditButton_Click(object sender, EventArgs e)
