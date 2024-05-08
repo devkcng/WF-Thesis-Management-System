7f0fd6513c8efa07a862f678a908bf0fc4e4f6aa
KienLoveNH <kientri098@gmail.com>
2024-05-08 12:36:08 +0700

    Re do UcTeacherAll Mark

--- WFThesisManagementSystem/Forms/TeacherViews/TeacherUserControl/UcTeacherAllMark.cs
+++ WFThesisManagementSystem/Forms/TeacherViews/TeacherUserControl/UcTeacherAllMark.cs
@@ -12,9 +12,16 @@ namespace WFThesisManagementSystem.Forms.TeacherViews.TeacherUserControl
 {
     public partial class UcTeacherAllMark : UserControl
     {
+        public event EventHandler cbxStudentbutton;
         public UcTeacherAllMark()
         {
             InitializeComponent();
+            cbxStudent.SelectedValueChanged += cbxStudent_SelectedIndexChanged;
+        }
+
+        private void cbxStudent_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cbxStudentbutton?.Invoke(this, EventArgs.Empty);
         }
     }
 }
